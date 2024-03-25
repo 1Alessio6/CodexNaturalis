5bed1e05aeeb94a47c0f77fcfd74832e800bfabe
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-03-25 08:06:30 +0100

    feat: add ChatDatabase class and related tests - add constructor and method to add and get the message list

---
+++ src/main/java/it/polimi/ingsw/model/chat/ChatDatabase.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.model.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Chat database to store all messages sent by players.
+ */
+public class ChatDatabase {
+    /**
+     * FIFO to store messages sent by players in order of their arrival.
+     */
+    private final List<Message> messageList;
+
+    public ChatDatabase() {
+        messageList = new ArrayList<>();
+    }
+
+    /**
+     * Stores the message.
+     *
+     * @param message to be stored.
+     * @throws IllegalArgumentException if message is null.
+     */
+    public void addMessage(Message message) throws IllegalArgumentException {
+        if (message == null) {
+            throw new IllegalArgumentException("Message cannot be null");
+        }
+        messageList.add(message);
+    }
+
+    /**
+     * Returns the messages sent by the players in order of arrival time: the oldest is the leftmost one.
+     *
+     * @return messages sent by the players.
+     */
+    public List<Message> getMessages() {
+        return messageList;
+    }
+}
