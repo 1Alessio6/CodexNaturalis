e9f0e90e492d8fad0f24f42609f38fcab5eb6ee0
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-01 17:18:10 +0200

    fix(Card): replace setters with constructor

--- src/main/java/it/polimi/ingsw/model/card/Card.java
+++ src/main/java/it/polimi/ingsw/model/card/Card.java
@@ -4,19 +4,17 @@ public class Card {
     private Front front;
     private Back back;
 
-    public Back getBack() {
-        return back;
+    public Card(Front front, Back back) {
+        this.front = front;
+        this.back = back;
     }
 
-    public void setBack(Back back) {
-        this.back = back;
+    public Back getBack() {
+        return back;
     }
 
     public Front getFront() {
         return front;
     }
 
-    public void setFront(Front front) {
-        this.front = front;
-    }
 }
