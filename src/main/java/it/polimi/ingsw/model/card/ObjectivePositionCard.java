2bb30243f2137d78834513396eaa340bdb66669b
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-01 16:48:33 +0200

    fix: add super() in ObjectivePositionCard and ObjectiveResourceCard to construct the base class attributes

--- src/main/java/it/polimi/ingsw/model/card/ObjectivePositionCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectivePositionCard.java
@@ -7,8 +7,12 @@ import java.util.Map;
 public final class ObjectivePositionCard extends ObjectiveCard {
     Map<Position, Color> condition;
 
-    public ObjectivePositionCard(Map<Position, Color> condition, int multiplier) {
+    public ObjectivePositionCard(Map<Position, Color> condition, int multiplier) throws IllegalArgumentException {
+        super(multiplier);
+
+        if (condition == null) {
+            throw new IllegalArgumentException("Condition cannot be null");
+        }
         this.condition = condition;
-        this.multiplier = multiplier;
     }
 }
--- src/main/java/it/polimi/ingsw/model/card/ObjectiveResourceCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectiveResourceCard.java
@@ -6,8 +6,13 @@ import java.util.Set;
 public final class ObjectiveResourceCard extends ObjectiveCard {
     List<Symbol> condition;
 
-    public ObjectiveResourceCard(List<Symbol> condition, int multiplier) {
+    public ObjectiveResourceCard(List<Symbol> condition, int multiplier) throws  IllegalArgumentException {
+        super(multiplier);
+
+        if (condition == null) {
+            throw new IllegalArgumentException("Condition cannot be nul");
+        }
+
         this.condition = condition;
-        this.multiplier = multiplier;
     }
 }
