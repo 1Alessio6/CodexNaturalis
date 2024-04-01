0ec26666af839e86e2a2053c91201031f438217a
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-01 17:15:26 +0200

    fix(Back)!: change resources representation to a Map and replace setters with constructor.

--- src/main/java/it/polimi/ingsw/model/card/Back.java
+++ src/main/java/it/polimi/ingsw/model/card/Back.java
@@ -1,15 +1,36 @@
 package it.polimi.ingsw.model.card;
 
-import java.util.Set;
+import java.util.List;
+import java.util.Map;
 
-public class Back extends Face{
-    private Set<Symbol> resources;
+public class Back extends Face {
+    private Map<Symbol, Integer> resources;
 
-    public Set<Symbol> getResources() {
+    /**
+     * Constructs a front card with the color, corners and resources provided.
+     * @param color of the card.
+     * @param corners of the card.
+     * @param resources provided by the card.
+     */
+    public Back (Color color, List<Corner> corners, Map<Symbol,Integer> resources) throws IllegalArgumentException {
+        super(color, corners);
+
+        if (resources == null) {
+            throw new IllegalArgumentException("Resources cannot be null");
+        }
+
+        for (Integer num  : resources.values()) {
+            if (num <= 0) {
+                throw new IllegalArgumentException("Symbol occurrences cannot be negative or zero, otherwise it wouldn't be a resource");
+            }
+        }
+
+        this.resources = resources;
+    }
+
+    public Map<Symbol, Integer> getResources() {
         return resources;
     }
 
-    public void setResources(Set<Symbol> resources) {
-        this.resources = resources;
-    }
+
 }
