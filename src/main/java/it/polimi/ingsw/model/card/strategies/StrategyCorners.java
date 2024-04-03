b68999f7fdf65c7da35aac1b492d003a3a7143b9
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-03 22:07:27 +0200

    feat(StrategyCorners): create corners logic

--- src/main/java/it/polimi/ingsw/model/card/strategies/StrategyCorners.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/StrategyCorners.java
@@ -1,12 +1,27 @@
 package it.polimi.ingsw.model.card.strategies;
 
+import it.polimi.ingsw.model.board.Availability;
 import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
+import it.polimi.ingsw.model.board.Tile;
 
-public class StrategyCorners implements CalculatePoints{
+import java.util.Map;
+
+public class StrategyCorners implements CalculatePoints {
 
     @Override
     public int calculatePoints(Position pos, Playground playground) {
-        return 0;
+        int multiplier = playground.getArea().get(pos).getFace().getScore();
+        Map<Position, Tile> area = playground.getArea();
+
+        return Math.toIntExact(
+                area
+                .keySet()
+                .stream()
+                .filter(p -> (Math.abs(p.getX()) <= 1)
+                        && (Math.abs(p.getY()) <= 1)
+                        && area.get(p).getAvailability()
+                        .equals(Availability.OCCUPIED))
+                .count());
     }
 }
