6770008873343d0785244dcc2ac9019a6c40230c
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-03 13:40:21 +0200

    feat(ObjectiveCard): replace setter with constructor

--- src/main/java/it/polimi/ingsw/model/card/ObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectiveCard.java
@@ -3,16 +3,15 @@ package it.polimi.ingsw.model.card;
 public abstract class ObjectiveCard {
     int multiplier;
 
+    public ObjectiveCard(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
     public void getObjective() {
     }
 
     public int getMultiplier() {
         return multiplier;
     }
-
-    public void setMultiplier(int multiplier) {
-        this.multiplier = multiplier;
-    }
-
 
 }
