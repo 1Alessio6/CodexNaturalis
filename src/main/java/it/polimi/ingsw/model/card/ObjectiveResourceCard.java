4e31de8e38de6e5ef7fefbf14a3407c5da4a02be
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-09 20:47:10 +0200

    feat(ObjectiveResourceCard):add calculatePoints method

--- src/main/java/it/polimi/ingsw/model/card/ObjectiveResourceCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectiveResourceCard.java
@@ -2,24 +2,60 @@ package it.polimi.ingsw.model.card;
 
 import it.polimi.ingsw.model.board.Playground;
 
-import java.util.List;
-import java.util.Set;
+import it.polimi.ingsw.model.board.Playground;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class ObjectiveResourceCard extends ObjectiveCard {
-    List<Symbol> condition;
+    Map<Symbol,Integer> condition=new HashMap<>();
 
-    public ObjectiveResourceCard(List<Symbol> condition, int multiplier) throws  IllegalArgumentException {
+    public ObjectiveResourceCard(Map<Symbol,Integer> condition, int multiplier) {
         super(multiplier);
-
-        if (condition == null) {
-            throw new IllegalArgumentException("Condition cannot be nul");
-        }
-
         this.condition = condition;
     }
 
     @Override
-    public int calculatePoints(Playground p) {
-        return 0;
+    public int calculatePoints(Playground playground) {
+
+        if(condition.size()==1) { // one type of symbol case
+
+            int c = 0;
+            // take the unique symbol condition present in condition HashMap
+            Object object = condition.keySet().toArray()[0];
+
+            if (playground.getResources().containsKey(object)) { //verify whether the symbol is contained or not in resources HashMap
+                //calculate the number of times that the condition is fulfilled and return the points
+                c = (int) Math.floor((double) playground.getResources().get(object) / condition.get(object));
+                return c * getMultiplier();
+
+            } else { //unfulfilled condition
+                return c;
+            }
+        }
+        else { //more than one type of symbol case
+
+            AtomicInteger counter=new AtomicInteger();
+            // create a copy of condition in temporalMap
+            HashMap<Symbol,Integer> temporalMap=new HashMap<>(condition);
+
+            temporalMap.forEach((k,v)->{ // for each k in temporalMap, temporalMap values are updated to the values contained in resources map
+                condition.put(k,playground.getResources().get(k));
+            });
+
+            temporalMap.forEach((k,v)->{ //update temporalMap values every time the condition is fulfilled
+                while(temporalMap.get(k)-condition.get(k)>0){
+                    condition.put(k,temporalMap.get(k)-condition.get(k));
+                    counter.getAndIncrement();
+                }
+            });
+
+            //return points
+            return counter.intValue()*getMultiplier();
+
+        }
     }
+
 }
+
+
