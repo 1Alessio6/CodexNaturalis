3fc934a871a76edfca356ca2ce1dd82625115f28
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-10 16:11:31 +0200

    feat(strategies): add CalculateBackPoints

---
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculateBackPoints.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model.card.strategies;
+
+import it.polimi.ingsw.model.board.Playground;
+import it.polimi.ingsw.model.board.Position;
+
+public class CalculateBackPoints implements CalculatePoints{
+    @Override
+    public int calculatePoints(Position pos, Playground playground) {
+
+        return 0;
+
+    }
+}
