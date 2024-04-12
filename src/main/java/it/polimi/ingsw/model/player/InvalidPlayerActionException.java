deafe24f9936888cc270164ac5d6ca1b8e0f16a7
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-12 12:33:14 +0200

    feat: create InvalidPlayerActionException to handle invalid player's actions

---
+++ src/main/java/it/polimi/ingsw/model/player/InvalidPlayerActionException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.model.player;
+
+public class InvalidPlayerActionException extends Exception {
+    InvalidPlayerActionException(String message) {
+        super(message);
+    }
+}
