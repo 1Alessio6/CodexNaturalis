8a4fc7fcb7171a24de6e2af2d1e40dbc565ed202
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-14 23:53:24 +0200

    refactor(Deck): modify some stuff - deck type defined by an enum - delete setter

--- src/main/java/it/polimi/ingsw/model/card/Deck.java
+++ src/main/java/it/polimi/ingsw/model/card/Deck.java
@@ -5,20 +5,16 @@ import java.util.Stack;
 
 public class Deck<T> {
     // todo. type?
-    String type;
-    Stack<T> deck;
+    private final DeckType type;
+    private final Stack<T> deck;
 
-    Deck(String type, Stack<T> deck) {
+    public Deck(DeckType type, Stack<T> deck) {
         this.type = type;
         this.deck = deck;
     }
 
-    public String getType() {
+    public DeckType getType() {
         return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 
     public T draw() throws EmptyDeckException {
