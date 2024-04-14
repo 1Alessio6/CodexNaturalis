6e580bb7a96d62afb93cf7372ef956abbffeec51
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-15 01:51:37 +0200

    feat(Front): add constructors for starting and resource fronts

--- src/main/java/it/polimi/ingsw/model/card/Front.java
+++ src/main/java/it/polimi/ingsw/model/card/Front.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.model.card;
 
 import it.polimi.ingsw.model.board.Playground;
+import it.polimi.ingsw.model.card.strategies.CalculateNoCondition;
 import it.polimi.ingsw.model.card.strategies.CalculatePoints;
 
 import java.util.Map;
-import java.util.List;
 
 public class Front extends Face {
     private final int score;
@@ -17,7 +17,17 @@ public class Front extends Face {
      * @param score   obtained after positioning the card.
      * @throws IllegalArgumentException if any argument is null or there are not 4 corners or the score is negative.
      */
-    Front(Color color, Map<CornerPosition, Corner> corners, int score, CalculatePoints calculator) throws IllegalArgumentException {
+    public Front(Color color, Map<CornerPosition, Corner> corners, int score) throws IllegalArgumentException {
+        super(color, corners, new CalculateNoCondition());
+
+        if (score < 0) {
+            throw new IllegalArgumentException("Score cannot be negative");
+        }
+
+        this.score = score;
+    }
+
+    public Front(Color color, Map<CornerPosition, Corner> corners, int score, CalculatePoints calculator) throws IllegalArgumentException {
         super(color, corners, calculator);
 
         if (score < 0) {
@@ -25,6 +35,17 @@ public class Front extends Face {
         }
 
         this.score = score;
+    }
+
+    /**
+     * ad hoc constructor for StartingCards
+     * @param corners of startingCard
+     * @throws IllegalArgumentException
+     */
+    public Front(Map<CornerPosition, Corner> corners) throws IllegalArgumentException {
+        super(null, corners, new CalculateNoCondition());
+
+        this.score = 0;
     }
 
     public int getScore() {
