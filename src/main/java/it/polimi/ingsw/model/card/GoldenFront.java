d9a0c44d57d5c619cf49b66e3acc61f451a47f3b
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-15 01:52:56 +0200

    refactor(GoldenFront): change constructor logic and set final attributes constructor manages calculator, as json only have condition

--- src/main/java/it/polimi/ingsw/model/card/GoldenFront.java
+++ src/main/java/it/polimi/ingsw/model/card/GoldenFront.java
@@ -1,13 +1,15 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.model.card.strategies.CalculateCorners;
+import it.polimi.ingsw.model.card.strategies.CalculateNoCondition;
 import it.polimi.ingsw.model.card.strategies.CalculatePoints;
+import it.polimi.ingsw.model.card.strategies.CalculateResources;
 
-import java.util.List;
 import java.util.Map;
 
 public final class GoldenFront extends Front {
-   private Condition pointsCondition; /* should be enum? */
-   private List<Symbol> requirements; /* set? */
+   private final Condition pointsCondition; /* should be enum? */
+   private final Map<Symbol, Integer> requirements; /* set? */
 
     /**
      * Constructs a front card with the color, score and corners provided.
@@ -17,19 +19,32 @@ public final class GoldenFront extends Front {
      * @param score   obtained after positioning the card.
      * @throws IllegalArgumentException if any argument is null or there are not 4 corners or the score is negative.
      */
-    public GoldenFront(Color color, Map<CornerPosition, Corner> corners, int score, CalculatePoints calculator) throws IllegalArgumentException {
-            super(color, corners, score, calculator);
+    public GoldenFront(Color color, Map<CornerPosition, Corner> corners, int score, Condition pointsCondition, Map<Symbol, Integer> requirements) throws IllegalArgumentException {
+        CalculatePoints calculator;
+        switch (pointsCondition){
+            case CORNERS:
+                calculator = new CalculateCorners();
+                break;
+            case NUM_MANUSCRIPT:
+            case NUM_INKWKELL:
+            case NUM_QUILL:
+                calculator = new CalculateResources();
+                break;
+            case null:
+                calculator = new CalculateNoCondition();
+                break;
+        }
+
+        super(color, corners, score, calculator);
+        this.pointsCondition = pointsCondition;
+        this.requirements = requirements;
     }
 
-    public List<Symbol> getRequirements() {
+    public Map<Symbol, Integer> getRequirements() {
         return requirements;
     }
 
     public Condition getPointsCondition() {
         return pointsCondition;
-    }
-
-    public void setPointsCondition(Condition pointsCondition) {
-        this.pointsCondition = pointsCondition;
     }
 }
