88ffa5c3236c989388e2d4d25651d7a758efa1af
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-14 20:56:44 +0200

    chore(strategies): rename from Strategy to Calculate

--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculateCorners.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculateCorners.java
@@ -3,14 +3,11 @@ package it.polimi.ingsw.model.card.strategies;
 import it.polimi.ingsw.model.board.Availability;
 import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
-import it.polimi.ingsw.model.board.Tile;
-
-import java.util.Map;
 
 /**
  * Strategy used for golden cards that add points based on number of corners covered after it's placement
  */
-public class StrategyCorners implements CalculatePoints {
+public class CalculateCorners implements CalculatePoints {
     @Override
     public int calculatePoints(Position pos, Playground playground) {
         int multiplier = playground.getTile(pos).getFace().getScore();
--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculateNoCondition.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculateNoCondition.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.model.card.strategies;
 import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
 
-public class StrategyNoCondition implements CalculatePoints{
+public class CalculateNoCondition implements CalculatePoints{
     @Override
     public int calculatePoints(Position pos, Playground playground) {
         return playground.getTile(pos).getFace().getScore();
