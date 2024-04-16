8f11a6e42c691ce50ee45a9862a9c6fbda7b46e3
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-04-16 18:55:36 +0200

    refactor(ObjectiveResourceCard): change calculatePoints() method implementation

--- src/main/java/it/polimi/ingsw/model/card/ObjectiveResourceCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectiveResourceCard.java
@@ -8,52 +8,26 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class ObjectiveResourceCard extends ObjectiveCard {
-    Map<Symbol,Integer> condition=new HashMap<>();
+    private final Map<Symbol,Integer> condition;
 
     public ObjectiveResourceCard(Map<Symbol,Integer> condition, int multiplier) {
         super(multiplier);
         this.condition = condition;
     }
 
-    @Override
-    public int calculatePoints(Playground playground) {
+    public int calculatePoints(Playground playground){
+        assert !condition.isEmpty();
 
-        if(condition.size()==1) { // one type of symbol case
-
-            int c = 0;
-            // take the unique symbol condition present in condition HashMap
-            Object object = condition.keySet().toArray()[0];
-
-            if (playground.getResources().containsKey(object)) { //verify whether the symbol is contained or not in resources HashMap
-                //calculate the number of times that the condition is fulfilled and return the points
-                c = (int) Math.floor((double) playground.getResources().get(object) / condition.get(object));
-                return c * getMultiplier();
-
-            } else { //unfulfilled condition
-                return c;
+        int min = Integer.MAX_VALUE;
+        for(Symbol s : condition.keySet()) {
+            if(playground.getResources().containsKey(s)){
+                min = Math.min(min , playground.getResources().get(s) / condition.get(s));
+            }
+            else{
+                return 0;
             }
         }
-        else { //more than one type of symbol case
-
-            AtomicInteger counter=new AtomicInteger();
-            // create a copy of condition in temporalMap
-            HashMap<Symbol,Integer> temporalMap=new HashMap<>(condition);
-
-            temporalMap.forEach((k,v)->{ // for each k in temporalMap, temporalMap values are updated to the values contained in resources map
-                condition.put(k,playground.getResources().get(k));
-            });
-
-            temporalMap.forEach((k,v)->{ //update temporalMap values every time the condition is fulfilled
-                while(temporalMap.get(k)-condition.get(k)>0){
-                    condition.put(k,temporalMap.get(k)-condition.get(k));
-                    counter.getAndIncrement();
-                }
-            });
-
-            //return points
-            return counter.intValue()*getMultiplier();
-
-        }
+        return multiplier * min;
     }
 
 }
