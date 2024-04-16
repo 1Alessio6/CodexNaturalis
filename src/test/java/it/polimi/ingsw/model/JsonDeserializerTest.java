2d94c8f197a7a41318a96f4d0f611b7b5727859e
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-16 22:16:11 +0200

    feat(tests): creat test for deserializing every deck

--- src/test/java/it/polimi/ingsw/model/JsonDeserializerTest.java
+++ src/test/java/it/polimi/ingsw/model/JsonDeserializerTest.java
@@ -2,24 +2,37 @@ package it.polimi.ingsw.model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import it.polimi.ingsw.model.board.Position;
 import it.polimi.ingsw.model.card.*;
 import it.polimi.ingsw.model.card.strategies.CalculatePoints;
 import it.polimi.ingsw.model.card.strategies.CalculateResources;
+import it.polimi.ingsw.model.jsondeserializer.CornerDeserializer;
+import it.polimi.ingsw.model.jsondeserializer.PositionDeserializer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.*;
 
 public class JsonDeserializerTest {
     private Gson gson;
+    private final static String goldenCardsPath = "/cards/goldenCards.json";
+    private final static String resourceCardsPath = "/cards/resourceCards.json";
+    private final static String startingCardsPath = "/cards/startingCards.json";
+    private final static String objectivePositionCardsPath = "/cards/objectivePositionCards.json";
+    private final static String objectiveResourceCardsPath = "/cards/objectiveResourceCards.json";
 
     @BeforeEach
     public void setUp() {
-        System.out.println("Setup");
         GsonBuilder builder = new GsonBuilder();
         builder.registerTypeAdapter(CalculatePoints.class, new JsonTestCode<>());
+        builder.registerTypeAdapter(Corner.class, new CornerDeserializer());
+        builder.registerTypeAdapter(Position.class, new PositionDeserializer());
         gson = builder.create();
     }
 
@@ -35,7 +48,6 @@ public class JsonDeserializerTest {
         );
 
         String frontJson = gson.toJson(front);
-        System.out.println(frontJson);
 
         Front f = gson.fromJson(frontJson, Front.class);
         Assertions.assertEquals(front.getColor(), Color.BLUE);
@@ -60,11 +72,33 @@ public class JsonDeserializerTest {
         );
 
         String cardJson = gson.toJson(c);
-        System.out.println(cardJson);
-
-//        Card cInv = gson.fromJson(cardJson, Card.class);
-//        Assertions.assertEquals(cInv.getFace(Side.FRONT).getColor(), Color.BLUE);
+        Card cInv = gson.fromJson(cardJson, Card.class);
+        Assertions.assertEquals(cInv.getFace(Side.FRONT).getColor(), Color.BLUE);
     }
 
+    @Test
+    void deserializeGoldenCardsFromFile() {
+        InputStream resourceAsStream = Corner.class.getResourceAsStream(goldenCardsPath);
+        Reader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+        ArrayList<Card> myCards = gson.fromJson(reader, new TypeToken<>(){});
+    }
+
+    @Test
+    void deserializeResourceCardsFromFile() {
+        InputStream resourceAsStream = Corner.class.getResourceAsStream(resourceCardsPath);
+        Reader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+        ArrayList<Card> myCards = gson.fromJson(reader, new TypeToken<>(){});
+    }
+
+    @Test
+    void deserializeObjectiveCardsFromFile() {
+        InputStream objectiveResourceAsStream = Corner.class.getResourceAsStream(resourceCardsPath);
+        Reader reader = new BufferedReader(new InputStreamReader(objectiveResourceAsStream));
+        ArrayList<ObjectiveResourceCard> objectiveResourceCards = gson.fromJson(reader, new TypeToken<ArrayList<ObjectiveResourceCard>>(){});
+
+        InputStream objectivePositionAsStream = Corner.class.getResourceAsStream(resourceCardsPath);
+        reader = new BufferedReader(new InputStreamReader(objectiveResourceAsStream));
+        ArrayList<ObjectivePositionCard> objectivePosition = gson.fromJson(reader, new TypeToken<ArrayList<ObjectivePositionCard>>(){});
+    }
 
 }
--- src/test/java/it/polimi/ingsw/model/JsonTestCode.java
+++ src/test/java/it/polimi/ingsw/model/JsonTestCode.java
@@ -1,25 +1,26 @@
 package it.polimi.ingsw.model;
 
 import com.google.gson.*;
+import it.polimi.ingsw.model.card.strategies.CalculatePoints;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 
-public class JsonTestCode<CalculatePoints> implements JsonSerializer<CalculatePoints>, JsonDeserializer<CalculatePoints> {
+public class JsonTestCode<T> implements JsonSerializer<T>, JsonDeserializer<T> {
 
-    public CalculatePoints deserialize(JsonElement jsonElement, Type type,
+    public T deserialize(JsonElement jsonElement, Type type,
                                        JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         String calculatorPackageString = type.getTypeName().substring(0, type.getTypeName().lastIndexOf('.') + 1).concat(jsonElement.getAsString());
 
         Class<CalculatePoints> mytype = getObjectClass(calculatorPackageString);
         try {
-            return mytype.getDeclaredConstructor().newInstance();
+            return (T) mytype.getDeclaredConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public JsonElement serialize(CalculatePoints jsonElement, Type type, JsonSerializationContext jsonSerializationContext) {
+    public JsonElement serialize(T jsonElement, Type type, JsonSerializationContext jsonSerializationContext) {
         return new JsonPrimitive(jsonElement.getClass().getSimpleName());
     }
 
