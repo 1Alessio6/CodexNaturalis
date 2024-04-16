b7d6e1750423d57b49b769a25de4204b2610c3b1
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-17 00:53:26 +0200

    refactor!(deserialization): change deserialization logic - Deck has static methods for the class to deserialize - import CalculatorDeserializer to src/main/java/...

--- src/main/java/it/polimi/ingsw/model/card/Deck.java
+++ src/main/java/it/polimi/ingsw/model/card/Deck.java
@@ -1,5 +1,19 @@
 package it.polimi.ingsw.model.card;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import it.polimi.ingsw.model.board.Position;
+import it.polimi.ingsw.model.card.strategies.CalculatePoints;
+import it.polimi.ingsw.model.jsondeserializer.CalculatorDeserializer;
+import it.polimi.ingsw.model.jsondeserializer.CornerDeserializer;
+import it.polimi.ingsw.model.jsondeserializer.PositionDeserializer;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
@@ -17,13 +31,55 @@ public class Deck<T> {
         deck.addAll(list);
     }
 
-
     public T draw() throws EmptyDeckException {
         if (deck.isEmpty()) {
             throw new EmptyDeckException("EMPTY DECK");
         }
 
         return deck.pop();
+    }
+
+    // TODO: repetition needed (?) because can't create static method of generic
+    public static ArrayList<Card> getPlayableCardListFromJson(String jsonPath){
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(CalculatePoints.class, new CalculatorDeserializer<>());
+        builder.registerTypeAdapter(Corner.class, new CornerDeserializer());
+        builder.registerTypeAdapter(Position.class, new PositionDeserializer());
+        Gson gson = builder.create();
+
+        InputStream resourceAsStream = Corner.class.getResourceAsStream(jsonPath);
+        Reader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+
+        // use ArrayList because TypeAdapter for generic interfaces don't allow TypeToken of list
+        return gson.fromJson(reader, new TypeToken<>(){});
+    }
+
+    public static ArrayList<ObjectiveResourceCard> getObjectiveResourceCardListFromJson(String jsonPath){
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(CalculatePoints.class, new CalculatorDeserializer<>());
+        builder.registerTypeAdapter(Corner.class, new CornerDeserializer());
+        builder.registerTypeAdapter(Position.class, new PositionDeserializer());
+        Gson gson = builder.create();
+
+        InputStream resourceAsStream = Corner.class.getResourceAsStream(jsonPath);
+        Reader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+
+        // use ArrayList because TypeAdapter for generic interfaces don't allow TypeToken of list
+        return gson.fromJson(reader, new TypeToken<>(){});
+    }
+
+    public static ArrayList<ObjectivePositionCard> getObjectivePositionCardListFromJson(String jsonPath){
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(CalculatePoints.class, new CalculatorDeserializer<>());
+        builder.registerTypeAdapter(Corner.class, new CornerDeserializer());
+        builder.registerTypeAdapter(Position.class, new PositionDeserializer());
+        Gson gson = builder.create();
+
+        InputStream resourceAsStream = Corner.class.getResourceAsStream(jsonPath);
+        Reader reader = new BufferedReader(new InputStreamReader(resourceAsStream));
+
+        // use ArrayList because TypeAdapter for generic interfaces don't allow TypeToken of list
+        return gson.fromJson(reader, new TypeToken<>(){});
     }
 
     public boolean isEmpty() {
