75bd4ad7e3860807bf5d335a538ac5be7ebd49e6
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 17:55:57 +0200

    feat(Condition): add JavaDoc to Condition enumeration

--- src/main/java/it/polimi/ingsw/model/card/Condition.java
+++ src/main/java/it/polimi/ingsw/model/card/Condition.java
@@ -1,8 +1,20 @@
 package it.polimi.ingsw.model.card;
 
 public enum Condition {
+    /**
+     * Num_Quill, number of the quills required in the condition
+     */
     NUM_QUILL,
+    /**
+     * Num_Manuscripts, number of manuscripts required in the condition
+     */
     NUM_MANUSCRIPT,
-    NUM_INKWKELL,
+    /**
+     * Num_Inkwell, number of inkwells required in the condition
+     */
+    NUM_INKWELL,
+    /**
+     * Corners, if the condition in order to obtain points is to cover a corner
+     */
     CORNERS
 }
