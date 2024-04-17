2cff16b98833f1298a75128883137e3b6135e24a
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 17:48:32 +0200

    feat(CalculatePoints): add JavaDoc to CalculatePoints class

--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculatePoints.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculatePoints.java
@@ -2,6 +2,10 @@ package it.polimi.ingsw.model.card.strategies;
 
 import it.polimi.ingsw.model.board.*;
 
+/**
+ * Calculate Points is an interface capable of calculating the points earned
+ * depending on the particular encountered.
+ */
 public interface CalculatePoints {
     public int calculatePoints (Position pos, Playground playground);
 }
