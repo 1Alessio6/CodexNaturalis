ab488ebfafac9210b325d0193fb79e2b2f02159b
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 19:28:58 +0200

    feat(CalculateBackPoints): update CalculateBackPoints JavaDoc

--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculateBackPoints.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculateBackPoints.java
@@ -6,7 +6,15 @@ import it.polimi.ingsw.model.board.Position;
 /**
  * Strategy used to calculate the points for all type of cards in its back side
  */
-public class CalculateBackPoints implements CalculatePoints{
+public class CalculateBackPoints implements CalculatePoints {
+
+    /**
+     * Calculate the points found on the front od the card.
+     *
+     * @param pos        in the playground.
+     * @param playground on which the calculation will be performed.
+     * @return 0: by default cards doesn't have points in its back.
+     */
     @Override
     public int calculatePoints(Position pos, Playground playground) {
 
