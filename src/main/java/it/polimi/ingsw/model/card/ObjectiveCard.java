7b7567e68ab2f3bef19d788be4975723caf94051
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 18:27:23 +0200

    feat(ObjectiveCard): add JavaDoc to ObjectiveCard class

--- src/main/java/it/polimi/ingsw/model/card/ObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectiveCard.java
@@ -2,9 +2,22 @@ package it.polimi.ingsw.model.card;
 
 import it.polimi.ingsw.model.board.Playground;
 
+/**
+ * Immutable class representing the objective card.
+ */
+
 public abstract class ObjectiveCard {
+
+    /**
+     * Represents the number of points won when card is placed.
+     */
     int multiplier;
 
+    /**
+     * Constructs an objective card with given provided.
+     *
+     * @param multiplier of the objective card
+     */
     public ObjectiveCard(int multiplier) {
         this.multiplier = multiplier;
     }
@@ -12,6 +25,11 @@ public abstract class ObjectiveCard {
     public void getObjective() {
     }
 
+    /**
+     * Returns the multiplier in the objective card
+     *
+     * @return the multiplier
+     */
     public int getMultiplier() {
         return multiplier;
     }
