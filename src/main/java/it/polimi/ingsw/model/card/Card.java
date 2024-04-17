3fcb5ce61be1fbacac562ced2d9ffdffdf3b69d5
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 17:52:36 +0200

    feat(Card): add JavaDoc to Card class

--- src/main/java/it/polimi/ingsw/model/card/Card.java
+++ src/main/java/it/polimi/ingsw/model/card/Card.java
@@ -1,15 +1,35 @@
 package it.polimi.ingsw.model.card;
 
 public class Card {
+
+    /**
+     * Represents the front of the card
+     */
     private Front front;
+    /**
+     * Represents the back of the card
+     */
     private Back back;
 
+
+    /**
+     * Constructs a Card with the front and the back provided.
+     *
+     * @param front of the card.
+     * @param back  of the card.
+     */
     public Card(Front front, Back back) {
         this.front = front;
         this.back = back;
     }
 
-    public Face getFace(Side side)  {
+    /**
+     * Returns the face of the face with the given side.
+     *
+     * @param side of the card.
+     * @return the face of the card.
+     */
+    public Face getFace(Side side) {
         if (side.equals(Side.FRONT))
             return front;
         else
