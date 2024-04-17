dfa637197b44f4f0c0bfcc384168c3635b406218
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 18:17:24 +0200

    feat(EmptyDeckException): add JavaDoc to EmptyDeckException

--- src/main/java/it/polimi/ingsw/model/card/EmptyDeckException.java
+++ src/main/java/it/polimi/ingsw/model/card/EmptyDeckException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.card;
 
+/**
+ * Empty Deck Exception is thrown when the deck of cards is empty.
+ */
 public class EmptyDeckException extends Exception {
     EmptyDeckException(String message) {
         super(message);
