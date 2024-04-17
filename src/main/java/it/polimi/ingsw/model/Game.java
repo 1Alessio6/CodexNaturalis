709dcdae0069e8a780ab4fd5ea8e4be817579753
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-17 17:31:45 +0200

    feat: add pseudo code for deserialization of json files in the constructor

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import it.polimi.ingsw.jsondeserializer.InterfaceAdaptor;
 import it.polimi.ingsw.model.board.*;
 import it.polimi.ingsw.model.card.*;
+import it.polimi.ingsw.model.card.strategies.CalculatePoints;
 import it.polimi.ingsw.model.chat.ChatDatabase;
 import it.polimi.ingsw.model.player.InvalidPlayerActionException;
 import it.polimi.ingsw.model.player.Player;
@@ -47,13 +51,38 @@ public class Game {
      */
 
     public Game(Map<String, Color> users) {
-        this.resourceCards = new Deck<>(Deck.getPlayableCardListFromJson(resourceCardsPath));
-        this.goldenCards = new Deck<>(Deck.getPlayableCardListFromJson(goldenCardsPath));
-        this.starterCards = new Deck<>(Deck.getPlayableCardListFromJson(startingCardsPath));
+        // code for deserialization:
+        GsonBuilder builder = new GsonBuilder().enableComplexMapKeySerialization();
 
-        ArrayList<ObjectiveCard> objectiveCardList = new ArrayList<>();
-        objectiveCardList.addAll(Deck.getObjectiveResourceCardListFromJson(objectiveResourceCardsPath));
-        objectiveCardList.addAll(Deck.getObjectivePositionCardListFromJson(objectivePositionCardsPath));
+        builder.registerTypeAdapter(CalculatePoints.class, new InterfaceAdaptor<>());
+        Gson gson = builder.create();
+
+        /*
+            pseudo code:
+                 List<GoldenFront> goldenFronts = createListOfGoldenCards();
+                 List<Back> listOfBack = createListOfBack();
+                 List<Front> fronsts = createListOfFront();
+                 goldenCards = []
+                 for i in range(0:41):
+                    goldenFront = goldenFronts.get(i);
+                    back = listOfBack.get(i);
+                    goldenCards.append(new Card(goldenFront, back))
+
+                 replace List<GoldenFront> to List<Front> and repeat the same algorithm for resource cards.
+
+                 replace List<GoldenFront> to List<ObjectivePositionCard> and repeat the same algorithm without considering back
+
+                 replace List<GoldenFront> to List<ObjectiveResourceCard> and repeat the same algorithm without considering back
+         */
+
+
+        //this.resourceCards = new Deck<>(Deck.getPlayableCardListFromJson(resourceCardsPath));
+        //this.goldenCards = new Deck<>(Deck.getPlayableCardListFromJson(goldenCardsPath));
+        //this.starterCards = new Deck<>(Deck.getPlayableCardListFromJson(startingCardsPath));
+
+        //ArrayList<ObjectiveCard> objectiveCardList = new ArrayList<>();
+        //objectiveCardList.addAll(Deck.getObjectiveResourceCardListFromJson(objectiveResourceCardsPath));
+        //objectiveCardList.addAll(Deck.getObjectivePositionCardListFromJson(objectivePositionCardsPath));
 
         this.objectiveCards = new Deck<>(objectiveCardList);
 
