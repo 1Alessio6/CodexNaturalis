9f2d1d9dce15057bb29d2ceb107b456154e59599
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 18:31:08 +0200

    feat(NonexistentPlayerException): add JavaDoc to NonexistentPlayerException

--- src/main/java/it/polimi/ingsw/model/NonexistentPlayerException.java
+++ src/main/java/it/polimi/ingsw/model/NonexistentPlayerException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model;
 
+/**
+ * Nonexistent Player Exception is thrown when the searched player doesn't exist.
+ */
 public class NonexistentPlayerException extends Exception{
     public NonexistentPlayerException(){super("The player doesn't exist");}
 }
