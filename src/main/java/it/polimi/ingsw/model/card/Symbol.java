272baa84a0f5a699f6338525370dabbfb46b8347
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 19:49:32 +0200

    feat(Symbol): add JavaDoc to Symbol enumeration

--- src/main/java/it/polimi/ingsw/model/card/Symbol.java
+++ src/main/java/it/polimi/ingsw/model/card/Symbol.java
@@ -1,11 +1,42 @@
 package it.polimi.ingsw.model.card;
 
+
+/**
+ * Enumeration representing the symbols that may occur.
+ */
 public enum Symbol {
+    /**
+     * Plant, refers to the resource that belong to the Plant kingdom.
+     */
     PLANT,
+
+    /**
+     * Fungi, refers to the resource that belong to the Fungi kingdom.
+     */
     FUNGI,
+
+    /**
+     * Animal, refers to the resource that belong to the Animal kingdom.
+     */
     ANIMAL,
+
+    /**
+     * Insect, refers to the resource that belong to the Insect kingdom.
+     */
     INSECT,
+
+    /**
+     * Quill, refers to the Quill object.
+     */
     QUILL,
+
+    /**
+     * Inkwell, refers to the Inkwell object.
+     */
     INKWELL,
+
+    /**
+     * Manuscript, refers to the Manuscript object.
+     */
     MANUSCRIPT
 }
