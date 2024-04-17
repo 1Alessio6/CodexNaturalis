8fb2088ad9b476002a160754a788af74a3dfa381
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-17 17:30:33 +0200

    feat: add class InterfaceAdaptor for serialization/deserialization of interfaces

---
+++ src/main/java/it/polimi/ingsw/jsondeserializer/InterfaceAdaptor.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.jsondeserializer;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+public class InterfaceAdaptor<T> implements JsonSerializer<T>, JsonDeserializer<T> {
+
+    private static final String CLASS_IDENTIFIER = "concreteType";
+
+    private static final String ATTRIBUTES = "attributes";
+
+    public T deserialize(JsonElement jsonElement, Type type,
+                         JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+        // find the json entry mapped to CLASS_IDENTIFIER  and get it as string
+        String className = jsonObject.get(CLASS_IDENTIFIER).getAsString();
+        Class objectClass = getObjectClass(className); // get the concrete type
+        return jsonDeserializationContext.deserialize(jsonObject.get(ATTRIBUTES), objectClass);
+    }
+
+    public JsonElement serialize(T objToSerialize, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty(CLASS_IDENTIFIER, objToSerialize.getClass().getName()); // get the class name not including the package
+        jsonObject.add(ATTRIBUTES, jsonSerializationContext.serialize(objToSerialize)); // jsonSerializationContext calls the serialization method
+        return jsonObject;
+    }
+
+    private Class getObjectClass(String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new JsonParseException(e.getMessage());
+        }
+    }
+}
+
