bd30f3e724927d44288a973142d8137f352d786b
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 18:00:17 +0200

    feat(CornerPosition): add JavaDoc to CornerPosition enumeration

--- src/main/java/it/polimi/ingsw/model/card/CornerPosition.java
+++ src/main/java/it/polimi/ingsw/model/card/CornerPosition.java
@@ -1,8 +1,20 @@
 package it.polimi.ingsw.model.card;
 
 public enum CornerPosition {
+    /**
+     * Top Left, refers to the top left corner of the card.
+     */
     TOP_LEFT,
+    /**
+     * Top right, refers to top right corner of the card.
+     */
     TOP_RIGHT,
+    /**
+     * Lower right, refers to the lower right corner of the card.
+     */
     LOWER_RIGHT,
+    /**
+     * Lower left, refers to the lower left corner of the card.
+     */
     LOWER_LEFT
 }
