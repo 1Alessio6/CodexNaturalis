670c990e3504a2fbca02bc12d6808a01aa14df31
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-17 17:45:29 +0200

    feat(CalculateBackPoints): add JavaDocs to Calculate Back Points class

--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculateBackPoints.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculateBackPoints.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.model.card.strategies;
 import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
 
+/**
+ * Strategy used to calculate the points for all type of cards in its back side
+ */
 public class CalculateBackPoints implements CalculatePoints{
     @Override
     public int calculatePoints(Position pos, Playground playground) {
