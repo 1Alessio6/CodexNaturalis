12a4477dab007c219e836bea75702f0c91941b6d
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-18 21:01:19 +0200

    feat(PlayerAction): add JavaDoc to PlayerAction class

--- src/main/java/it/polimi/ingsw/model/player/PlayerAction.java
+++ src/main/java/it/polimi/ingsw/model/player/PlayerAction.java
@@ -18,14 +18,43 @@ import java.util.stream.Collectors;
 // state design pattern: PlayerAction is the current player's state.
 public abstract class PlayerAction {
 
+
+    /**
+     * Allows a player to place a card on the side and position specified.
+     *
+     * @param player   who performs the placement.
+     * @param card     to place.
+     * @param side     of the card.
+     * @param position in the playground.
+     * @return TODO
+     * @throws InvalidPlayerActionException            if the player cannot perform the operation.
+     * @throws Playground.UnavailablePositionException if the position is not available.
+     * @throws Playground.NotEnoughResourcesException  if the player's resources are not enough to place the card.
+     */
     int placeCard(Player player, Card card, Side side, Position position) throws InvalidPlayerActionException, Playground.UnavailablePositionException, Playground.NotEnoughResourcesException {
         throw new InvalidPlayerActionException("Player cannot place a card");
     }
 
+
+    /**
+     * Allows the player to place the secret objective card.
+     *
+     * @param player          who performs the placement.
+     * @param chosenObjective card.
+     * @throws InvalidPlayerActionException if the player cannot perform the operation.
+     */
     void placeObjectiveCard(Player player, int chosenObjective) throws InvalidPlayerActionException {
         throw new InvalidPlayerActionException("Player has already choose an objective");
     }
 
+
+    /**
+     * Adds a new card to the player's card list.
+     *
+     * @param player  to which the card is added.
+     * @param newCard to be added.
+     * @throws InvalidPlayerActionException if the player cannot perform the operation.
+     */
     void addCard(Player player, Card newCard) throws InvalidPlayerActionException {
         throw new InvalidPlayerActionException("Player cannot draw");
     }
@@ -38,17 +67,39 @@ public abstract class PlayerAction {
     //    throw new InvalidPlayerActionException("Player cannot draw");
     //}
 
+
+    /**
+     * Updates the state of the Player Action.
+     *
+     * @param player     to whom the update is applied.
+     * @param nextAction of the Player Action.
+     */
     void nextAction(Player player, PlayerAction nextAction) {
         player.setAction(nextAction);
     }
 
+
+    /**
+     * Returns true if the set-up has finished.
+     *
+     * @return false: by default the set-up is not fully established until the player has chosen the secret objective,
+     * in the ChooseObjective PlayerAction.
+     */
     boolean isSetupFinished() {
         return false;
     }
 
 }
 
+
+/**
+ * This class is responsible for choosing the starter card side and its placement.
+ */
 class ChooseStarter extends PlayerAction {
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     int placeCard(Player player, Card card, Side side, Position position)
             throws Playground.UnavailablePositionException, Playground.NotEnoughResourcesException {
@@ -60,7 +111,15 @@ class ChooseStarter extends PlayerAction {
     }
 }
 
+
+/**
+ * This class is responsible for managing the choice of the secret objective card.
+ */
 class ChooseObjective extends PlayerAction {
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     void placeObjectiveCard(Player player, int chosenObjective) throws InvalidPlayerActionException {
         List<ObjectiveCard> objectiveCards = player.getObjectives();
@@ -75,7 +134,16 @@ class ChooseObjective extends PlayerAction {
     }
 }
 
+
+/**
+ * This class is responsible for the placement of all the cards from the moment in which the secret objective card is
+ * chosen until the moment in which the last card is placed at the end of the game.
+ */
 class Place extends PlayerAction {
+
+    /**
+     * @inheritDoc}
+     */
     @Override
     int placeCard(Player player, Card card, Side side, Position position)
             throws Playground.UnavailablePositionException, Playground.NotEnoughResourcesException {
@@ -87,30 +155,57 @@ class Place extends PlayerAction {
         return player.getPoints();
     }
 
+
+    /**
+     * Returns true if the set-up has finished.
+     *
+     * @return true: set-up has finished in place PlayerAction's state.
+     */
     @Override
     boolean isSetupFinished() {
         return true;
     }
 }
 
+
+/**
+ * This class is responsible for drawing all kind of cards from the moment in which the secret objective card is
+ * chosen until the moment in which the last card is drawn before the end of the game.
+ */
 class Draw extends PlayerAction {
 
 
+    /**
+     * Private method that consents to add a new card to the player's card list.
+     *
+     * @param player to which the card is added.
+     * @param card   tobe added.
+     */
     private void addNewCard(Player player, Card card) {
         List<Card> cards = player.getCards();
-        assert(cards.size() == 2);
+        assert (cards.size() == 2);
         cards.add(card);
     }
 
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     void addCard(Player player, Card newCard) throws InvalidPlayerActionException {
         List<Card> cards = player.getCards();
-        assert(cards.size() == 2);
+        assert (cards.size() == 2);
         cards.add(newCard);
 
         nextAction(player, new Place());
     }
 
+
+    /**
+     * Returns true if the set-up has finished.
+     *
+     * @return true: set-up has finished in draw PlayerAction's state.
+     */
     @Override
     boolean isSetupFinished() {
         return true;
