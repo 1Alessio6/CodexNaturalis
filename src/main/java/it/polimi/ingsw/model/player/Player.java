e3b787d98bb6ff6abea081b896d923191a7b8acb
Natalia <112645876+audlo@users.noreply.github.com>
2024-04-18 21:33:50 +0200

    feat(Player): add JavaDoc to Player class

--- src/main/java/it/polimi/ingsw/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -10,26 +10,77 @@ import java.util.*;
 
 // designPattern(State, Player) == Contextor
 // The network status is not modelled as a state to avoid storing information related to the previous state to recovery the last state
+
+/**
+ * Represents the player.
+ */
 public class Player {
 
     //attributes
 
+
+    /**
+     * Indicates the username of the player.
+     */
     private String username;
+
+    /**
+     * Represents the playground in which the actions are performed.
+     */
     private Playground playground;
+
+    /**
+     * Represents the player's token color.
+     */
     private Color colour;
+
+    /**
+     * Specifies whether the player is connected or not.
+     */
     private boolean isConnected;
+
+    /**
+     * Represents the player's starter card.
+     */
     private Card starter;
+
+    /**
+     * Represents the list of cards in player's possession, excluding the secret objective card and the starter card.
+     */
     private List<Card> cards;
+
+    /**
+     * Represents the list of objective cards in player's possession.
+     */
     private List<ObjectiveCard> objectiveCards;
+
+    /**
+     * Represents the current player's PlayerAction.
+     */
     private PlayerAction playerAction;
 
+
+    /**
+     * Specifies if the player is active or not.
+     */
     private boolean isActive;
 
+
+    /**
+     * Constructs a player using the username, token color, starter card, list of objective cards to choose and a list
+     * with all other cards to be in player's possession provided.
+     *
+     * @param username           of the player.
+     * @param color              of the player's token.
+     * @param starter            card.
+     * @param cards              of the player.
+     * @param objectivesToChoose list.
+     */
     public Player(String username,
-           Color color,
-           Card starter,
-           List<Card> cards,
-           List<ObjectiveCard> objectivesToChoose) {
+                  Color color,
+                  Card starter,
+                  List<Card> cards,
+                  List<ObjectiveCard> objectivesToChoose) {
         int numCards = 3, numObjectives = 2;
 
         assert (cards.size() == numCards
@@ -48,35 +99,73 @@ public class Player {
 
     //methods
 
+
+    /**
+     * Return the current points present in the playground.
+     *
+     * @return current points.
+     */
     public int getPoints() {
         return playground.getPoints();
     }
 
+
+    /**
+     * Returns the username of the player.
+     *
+     * @return username.
+     */
     public String getUsername() {
         return username;
     }
 
+
+    /**
+     * Returns the current PlayerAction state of the player.
+     *
+     * @return PlayerAction.
+     */
     public PlayerAction getPlayerAction() {
         return playerAction;
     }
 
+
+    /**
+     * Returns the available positions present in the playground.
+     *
+     * @return a list with the available positions present in the playground.
+     */
     public List<Position> getAvailablePositions() {
         return playground.getAvailablePositions();
     }
 
     /**
      * Sets network <code>status</code> of this player.
+     *
      * @param networkStatus true if the player is connected, false otherwise.
      */
     public void setNetworkStatus(boolean networkStatus) {
         this.isConnected = networkStatus;
     }
 
+
+    /**
+     * Tests if the player is connected or not.
+     *
+     * @return true if the player is connected, false otherwise.
+     */
     public boolean isConnected() {
         return isConnected;
     }
 
     // Returns true iff the player has the specified <code>card</code>.
+
+    /**
+     * Tests if the player has the specified card.
+     *
+     * @param card to be tested.
+     * @return true if the player has the specified card, false otherwise.
+     */
     boolean has(Card card) {
         return cards.stream()
                 .filter((c) -> {
@@ -87,22 +176,52 @@ public class Player {
     }
 
     // discard the card from the player's hand
+
+    /**
+     * Discards the card from the player's hand.
+     *
+     * @param card to be discarded.
+     */
     void discard(Card card) {
         assert (cards.remove(card)); // the card must exist
     }
 
+
+    /**
+     * Sets the PlayerAction state of the PlayerAction
+     *
+     * @param nextPlayerAction to be set.
+     */
     void setAction(PlayerAction nextPlayerAction) {
         playerAction = nextPlayerAction;
     }
 
+
+    /**
+     * Returns the playground to the player.
+     *
+     * @return the playground.
+     */
     Playground getPlayground() {
         return playground;
     }
 
+
+    /**
+     * Returns player's objective cards.
+     *
+     * @return a list of objective cards.
+     */
     List<ObjectiveCard> getObjectives() {
         return objectiveCards;
     }
 
+
+    /**
+     * Returns player's cards, excluding the objective and starter cards.
+     *
+     * @return a list of cards.
+     */
     public List<Card> getCards() {
         return cards;
     }
@@ -138,15 +257,22 @@ public class Player {
         return playerAction.placeCard(this, card, side, position);
     }
 
+
+    /**
+     * Tests if the player has finished the set-up.
+     *
+     * @return true if the player has finished the set-up, false otherwise.
+     */
     public boolean isSetupFinished() {
         return playerAction.isSetupFinished();
     }
 
     /**
      * Adds a new card to this player.
+     *
      * @param newCard to add.
      * @throws InvalidPlayerActionException if the player cannot perform such  action.
-     * For example, the player has not already decided which card to place, thus they're not allowed to draw.
+     *                                      For example, the player has not already decided which card to place, thus they're not allowed to draw.
      */
     public void addCard(Card newCard) throws InvalidPlayerActionException {
         playerAction.addCard(this, newCard);
