bad9310fa481e2e523091d0c4cf89ed56063199d
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-04-23 11:16:28 +0200

    fix(playerAction): initialize playerAction attribute set final and add initialization inside the constructor

--- src/main/java/it/polimi/ingsw/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/model/player/Player.java
@@ -57,7 +57,7 @@ public class Player {
     /**
      * Represents the current player's PlayerAction.
      */
-    private PlayerAction playerAction;
+    private final PlayerAction playerAction;
 
 
     /**
@@ -95,6 +95,8 @@ public class Player {
 
         this.isConnected = true;
         this.playground = new Playground();
+
+        this.playerAction = new ChooseStarter();
     }
 
     //methods
