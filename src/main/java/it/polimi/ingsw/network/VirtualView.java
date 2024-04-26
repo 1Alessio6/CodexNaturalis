72c3eeb1946ebe169d63ad170ef90a29e322aed1
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-04-26 16:49:12 +0200

    feat(VirtualServer): add all notify methods needed to complete the Observer pattern

--- src/main/java/it/polimi/ingsw/network/VirtualView.java
+++ src/main/java/it/polimi/ingsw/network/VirtualView.java
@@ -17,46 +17,46 @@ public interface VirtualView extends Remote {
 
 
     //method to show players base information
-    void showPlayerUsername(String username) throws RemoteException;
+    public void showPlayerUsername(String username) throws RemoteException;
 
-    void showUpdatePlayerStatus(boolean isConnected) throws RemoteException;
+    public void showUpdatePlayerStatus(boolean isConnected) throws RemoteException;
 
-    void showColor(Color color) throws RemoteException;
+    public void showColor(Color color) throws RemoteException;
 
-    void showRemainingColor(Set<Color> remainingColor) throws RemoteException;
+    public void showRemainingColor(Set<Color> remainingColor) throws RemoteException;
 
     //method to show updated information after a place
 
     //it's more efficient to pass only the new added tile and position in order to avoid sending the whole map to the client every time
-    void showUpdatePlaygroundArea(Position position, Tile tile) throws RemoteException;
+    public void showUpdatePlaygroundArea(Position position, Tile tile) throws RemoteException;
 
-    void showUpdatePoints(int points) throws RemoteException;
+    public void showUpdatePoints(int points) throws RemoteException;
 
-    void showUpdateResources(Symbol symbol, int totalAmount) throws RemoteException;
+    public void showUpdateResources(Symbol symbol, int totalAmount) throws RemoteException;
 
     //method to show update after a draw
 
-    void showUpdatePlayerCards(List<Card> newCards) throws RemoteException;
+    public void showUpdatePlayerCards(List<Card> newCards) throws RemoteException;
 
-    void showUpdateDeck(boolean isEmpty) throws RemoteException; //the client (player) can't see the updates of a deck till the deck is empty
+    public void showUpdateDeck(boolean isEmpty) throws RemoteException; //the client (player) can't see the updates of a deck till the deck is empty
 
-    void showUpdateFaceUpCards(int position, Card card) throws RemoteException;
+    public void showUpdateFaceUpCards(int position, Card card) throws RemoteException;
     //Face up cards have specific position inside the list, so it's possible to pass only the position which needs to be updated
 
-    void showCommonObjectiveCard(List<ObjectiveCard> commonObjective) throws RemoteException;
+    public void showCommonObjectiveCard(List<ObjectiveCard> commonObjective) throws RemoteException;
 
-    void showUpdatePlayerObjectiveCard(List<ObjectiveCard> privateObjective) throws RemoteException;
+    public void showUpdatePlayerObjectiveCard(List<ObjectiveCard> privateObjective) throws RemoteException;
     //it's the only method that show information to one player per time
 
-    void showPlayerStarterCard(Card starterCard) throws RemoteException;
+    public void showPlayerStarterCard(Card starterCard) throws RemoteException;
 
-    void showUpdateChat(Message message) throws RemoteException;
+    public void showUpdateChat(Message message) throws RemoteException;
 
-    void showUpdateCurrentPlayer(Player currentPlayer) throws RemoteException;
+    public void showUpdateCurrentPlayer(Player currentPlayer) throws RemoteException;
 
-    void showUpdateGamePhase(String GamePhase) throws RemoteException;
+    public void showUpdateGamePhase(String GamePhase) throws RemoteException;
 
-    void reportError(String details) throws RemoteException;
+    public void reportError(String details) throws RemoteException;
 
     //todo check if showUpdatedTimer is required or two synchronized timer are used for both server and client
 
