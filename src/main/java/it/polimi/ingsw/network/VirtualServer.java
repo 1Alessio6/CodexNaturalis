7a007686e1eaa303741bd973f94632475e6f9df2
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-04-26 16:30:34 +0200

    refactor(VirtualServer): change from interface to abstract class in order to follow Observer pattern

--- src/main/java/it/polimi/ingsw/network/VirtualServer.java
+++ src/main/java/it/polimi/ingsw/network/VirtualServer.java
@@ -2,15 +2,20 @@ package it.polimi.ingsw.network;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 /*
 todo: in order to follow the design pattern this should be an abstract class with a list of listeners. we have to decide
- if we prefer to maintain the pattern and change into an abstract class or if we add the list of VirtualView only in the rmi and socket server
+ if we prefer to maintain the pattern or if we add the list of VirtualView only in the rmi and socket server
  */
 
 
-public interface VirtualServer extends Remote {
+public abstract class VirtualServer implements Remote {
 
-    public void connect(VirtualView client) throws RemoteException; //possible to add username as a parameter
+    List<VirtualView> connectedClients;
+
+    public abstract void connect(VirtualView client) throws RemoteException; //possible to add username as a parameter
+
+
 
 }
