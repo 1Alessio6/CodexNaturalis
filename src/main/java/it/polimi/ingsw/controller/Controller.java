5f36fd9294126f3787f6e2860e8fc85a424d6608
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-04-27 20:37:02 +0200

    feat(Controller): add methods

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -3,17 +3,24 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.model.Deck.DeckType;
 import it.polimi.ingsw.model.InvalidGamePhaseException;
 import it.polimi.ingsw.model.NonexistentPlayerException;
+import it.polimi.ingsw.model.SuspendedGameException;
 import it.polimi.ingsw.model.board.Playground;
+import it.polimi.ingsw.model.card.Card;
 import it.polimi.ingsw.model.card.Color.InvalidColorException;
 import it.polimi.ingsw.model.card.Color.PlayerColor;
 import it.polimi.ingsw.model.card.EmptyDeckException;
+import it.polimi.ingsw.model.chat.message.InvalidMessageException;
+import it.polimi.ingsw.model.chat.message.Message;
+import it.polimi.ingsw.model.lobby.AlreadyInLobbyException;
+import it.polimi.ingsw.model.lobby.FullLobbyException;
+import it.polimi.ingsw.model.lobby.Lobby;
 import it.polimi.ingsw.model.player.InvalidPlayerActionException;
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.model.board.Position;
 import it.polimi.ingsw.model.card.Side;
 
 import java.util.EventListener;
-import java.util.Timer;
+import java.util.List;
 
 /**
  * The Controller class manages the entry of players through the use of a lobby, as well as their subsequent
@@ -21,171 +28,157 @@ import java.util.Timer;
  * It also allows the player to make moves in the playground and to choose his/her own username and token color
  * before the start of a game.
  */
-// todo. controller to fix
 public class Controller implements EventListener {
-    /**
-     * Measures the time remaining before the proclamation of the only player present in the game as the winner
-     * in the event of a net collapse or client crash.
-     */
-    private Timer timer;
-    /**
-     * Manage the entry of the players into the game.
-     */
-   // private Lobby lobby;
-    /**
-     * Representation of the game.
-     */
+    private Lobby lobby;
     private Game game;
+    private List<String> usernames;
 
-    /**
-     * Constructs the controller.
-     */
     public Controller() {
+        lobby = new Lobby();
     }
 
     /**
-     * Setting of the timer duration time written in milliseconds.
-     */
-    private static final long countdown = 60000;
-    // event listener
-
-    /**
-     * Places the card on the side and position specified.
+     * Joins user to the lobby.
      *
-     * @param cardIdx  specifying the card.
-     * @param side     of the card.
-     * @param pos      of the playground.
-     * @param username of the player.
+     * @param username the user's name who joins the lobby.
+     * @return true if the game starts after the user has joined the lobby.
+     * @throws FullLobbyException      if the lobby is full.
+     * @throws AlreadyInLobbyException if there's already a player with <code>username</code> in the lobby.
      */
-    public void placeCard(int cardIdx, Side side, Position pos, String username) {
-        try {
-            game.placeCard(username, game.getPlayerByUsername(username).getCards().get(cardIdx), side, pos);
-        } catch (InvalidPlayerActionException e) {
-            throw new RuntimeException(e);
-        } catch (Playground.UnavailablePositionException e) {
-            throw new RuntimeException(e);
-        } catch (Playground.NotEnoughResourcesException e) {
-            throw new RuntimeException(e);
-        } catch (InvalidGamePhaseException e) {
-            throw new RuntimeException(e);
-        }
+    public boolean joinLobby(String username) throws FullLobbyException, AlreadyInLobbyException {
+        game = lobby.joinLobby(username);
+        return game != null;
     }
 
-    /**
-     * Draws from one of the available face up cards.
-     *
-     * @param faceUpCardIdx specifying the face up card.
-     * @param username      of the player.
-     */
-    public void drawFromFaceUpCards(int faceUpCardIdx, String username) {
-        try {
-            game.drawFromFaceUpCards(username, faceUpCardIdx);
-        } catch (InvalidPlayerActionException | InvalidGamePhaseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Draws a resource card or a golden card from the deck.
-     *
-     * @param deckType the type of the deck.
-     * @param username of the player.
-     */
-    public void drawFromDeck(String username, DeckType deckType) {
-        try {
-            game.drawFromDeck(username, deckType);
-        } catch (EmptyDeckException e) {
-            e.printStackTrace();
-        } catch (InvalidPlayerActionException e) {
-            e.printStackTrace();
-        } catch (InvalidGamePhaseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Allows the connection of a player into the game.
-     *
-     * @param username of the player.
-     */
+    // fixme(return_value): it has to be the client's representation of a player to recovery their state.
     public void joinGame(String username) {
         game.setNetworkStatus(username, true);
+        // todo. get all info needed by the client related to the player and return them.
     }
 
     /**
-     * Allows the player to join into the lobby.
+     * Removes the user from the lobby.
      *
-     * @param username of the player.
-     * @param color    token color chosen for the player.
+     * @param username the user's name who leaves the lobby.
+     * @return true if the lobby has been reset, false otherwise.
      */
-  //  public void joinLobby(String username, Color color) {
-  //      lobby.addPlayer(username, color);
-  //  }
-
-  //  /**
-  //   * Creates a new game with the players in the lobby.
-  //   */
-  //  public void createGame() {
-  //      try {
-  //          this.game = this.lobby.createGame();
-  //      } catch (IllegalStateException e) {
-  //          e.printStackTrace();
-  //      }
-  //  }
-
-    /**
-     *todo
-     */
-    public void playerDisconnected() {
-        //timer.schedule(game.setFinished(), countdown);
-        timer.cancel();
+    public boolean leaveLobby(String username) {
+        List<String> leftUsers = lobby.remove(username);
+        return leftUsers.isEmpty();
     }
 
     /**
-     * Creates a lobby.
+     * Removes the user from the game.
      *
-     * @param creator    of the lobby.
-     * @param color      of the creator.
-     * @param numPlayers required to start the game.
+     * @param username the user's name.
+     * @return true if there's no enough player, so the game has to be suspended; false otherwise.
      */
-  //  public void createLobby(String creator, Color color, int numPlayers) {
-  //      this.lobby = new Lobby(creator, numPlayers);
-  //  }
+    public boolean leaveGame(String username) {
+        game.setNetworkStatus(username, false);
+        // return true if the game need to be suspended.
+        return game.getActivePlayers().size() <= 1;
+    }
+
+    /**
+     * Places the starter card of the player in the chosen side.
+     *
+     * @param username the player's username.
+     * @param side     the side of the starter card.
+     * @throws InvalidPlayerActionException if the player cannot perform this action.
+     * @throws InvalidGamePhaseException    if the player has already finished their setup.
+     */
+    public void placeStarter(String username, Side side) throws InvalidPlayerActionException, InvalidGamePhaseException {
+        game.placeStarter(username, side);
+    }
+
+    /**
+     * Assigns color to the player with <code>username</code>.
+     *
+     * @param username of the player who have chosen the color.
+     * @param color    chosen by the player.
+     * @return the list of remaining colors after the player's choice.
+     * @throws InvalidColorException        if the color has already been chosen.
+     * @throws InvalidPlayerActionException if the player cannot perform this action.
+     * @throws InvalidGamePhaseException    if the player has already finished their setup.
+     */
+    public List<PlayerColor> assignColor(String username, PlayerColor color) throws NonexistentPlayerException, InvalidColorException, InvalidPlayerActionException, InvalidGamePhaseException {
+        return game.assignColor(username, color);
+    }
 
     /**
      * Places the secret objective from one of the two available.
      *
-     * @param objectiveIdx specifying the secret card.
-     * @param username     of the player.
+     * @param username        of the player.
+     * @param chosenObjective the chosen objective.
+     * @throws InvalidPlayerActionException if the player cannot perform the operation. For example the player has already chosen the objective.
+     * @throws InvalidGamePhaseException    if the player has already finished the setup.
      */
-    public void chooseObjectiveCard(int objectiveIdx, String username) throws InvalidPlayerActionException {
-        try {
-            game.placeObjectiveCard(username, objectiveIdx);
-        } catch (InvalidPlayerActionException | InvalidGamePhaseException e) {
-            e.printStackTrace();
-        }
-
+    public void placeObjectiveCard(String username, int chosenObjective) throws InvalidPlayerActionException, InvalidGamePhaseException {
+        game.placeObjectiveCard(username, chosenObjective);
     }
-
-  //  /**
-  //   * Assigns a name to the player.
-  //   *
-  //   * @param username of the player.
-  //   */
-  //  public void nameUsername(String username) {
-  //      lobby.addPlayer(username, null);
-  //  }
-  //  //assuming that a "NULL" value is going to be assigned in the color's enumeration
 
     /**
-     * Assigns a color for the player.
+     * Places the card on the side and position specified.
      *
      * @param username of the player.
-     * @param color    token color chosen by the player.
+     * @param idCard   to place.
+     * @param side     of the card.
+     * @param position in the playground.
+     * @throws InvalidPlayerActionException            if the player cannot perform the operation.
+     * @throws Playground.UnavailablePositionException if the position is not available. For example the player is trying to place the card in an already covered corner.
+     * @throws Playground.NotEnoughResourcesException  if the player's resource are not enough to place the card.
+     * @throws InvalidGamePhaseException               if the game phase cannot allow placing cards.
+     * @throws SuspendedGameException                  if the game is suspended, thus no placement is allowed.
      */
-    // todo. to improve exceptions handling.
-    public void assignColor(String username, PlayerColor color) throws NonexistentPlayerException, InvalidPlayerActionException, InvalidColorException, InvalidGamePhaseException {
-        game.assignColor(username, color);
+    public int placeCard(String username, int idCard, Side side, Position position) throws InvalidPlayerActionException, Playground.UnavailablePositionException, Playground.NotEnoughResourcesException, InvalidGamePhaseException, SuspendedGameException {
+        Card cardToPlace = game.getCard(username, idCard);
+        return game.placeCard(username, cardToPlace, side, position);
     }
 
+    /**
+     * Draws a card from the selected place.
+     *
+     * @param username the username of the player.
+     * @param idToDraw the id representing the deck or any of the face up card positions.
+     * @throws InvalidPlayerActionException if the player cannot perform the operation.
+     * @throws EmptyDeckException           if the selected deck is empty.
+     * @throws InvalidGamePhaseException    if the game phase doesn't allow the operation.
+     */
+    public void draw(String username, int idToDraw) throws InvalidPlayerActionException, EmptyDeckException, InvalidGamePhaseException {
+        if (idToDraw == 4) {
+            game.drawFromDeck(username, DeckType.GOLDEN);
+        } else if (idToDraw == 5) {
+            game.drawFromDeck(username, DeckType.RESOURCE);
+        } else {
+            game.drawFromFaceUpCards(username, idToDraw);
+        }
+    }
+
+    /**
+     * Gets the current player from the game.
+     *
+     * @return the current player's name.
+     */
+    public String getCurrentPlayer() {
+        return game.getCurrentPlayer().getUsername();
+    }
+
+
+    /**
+     * Skips the current player.
+     */
+    public void skipTurn() {
+        game.skipTurn();
+    }
+
+    /**
+     * Sends message from the author.
+     *
+     * @param author  of the message.
+     * @param message sent by the author.
+     * @throws InvalidMessageException if the message is invalid.
+     */
+    public void sendMessage(String author, Message message) throws InvalidMessageException {
+        game.registerMessage(author, message);
+    }
 }
