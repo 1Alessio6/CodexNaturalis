eaba559bf661836ff0dc4710c0c2b85857ba19fe
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-09 23:17:23 +0200

    refactor: change ClientGame for similarity with the Game (model)

--- src/main/java/it/polimi/ingsw/network/client/model/ClientGame.java
+++ src/main/java/it/polimi/ingsw/network/client/model/ClientGame.java
@@ -1,16 +1,49 @@
 package it.polimi.ingsw.network.client.model;
 
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.chat.message.Message;
+import it.polimi.ingsw.network.client.model.board.ClientBoard;
+import it.polimi.ingsw.network.client.model.card.ClientCard;
+import it.polimi.ingsw.network.client.model.player.ClientPlayer;
+
 import java.util.List;
 
 public class ClientGame {
-    public ClientPlayer player;
+    private List<ClientCard> faceUpCards;
+    private List<ClientCard> commonObjects;
+    private int currentPlayerIdx; // index in the current player list.
+    private List<ClientPlayer> players;
+    private List<Message> messages;
+    private ClientBoard clientBoard;
 
-    public ClientPhase currentPhase;
+    public List<ClientCard> getFaceUpCards() {
+        return faceUpCards;
+    }
 
-    public List<ClientPlayer> otherPlayers;
+    public List<ClientCard> getCommonObjects() {
+        return commonObjects;
+    }
 
-    public int[] hiddenObjectivesID; //max 2 and cannot be into ClientPlayer class because it would be visible to all the game partecipants
+    public int getCurrentPlayerIdx() {
+        return currentPlayerIdx;
+    }
 
-    public ClientBoard clientBoard;
+    public List<ClientPlayer> getPlayers() {
+        return players;
+    }
 
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public ClientBoard getClientBoard() {
+        return clientBoard;
+    }
+
+    public ClientGame(Game game) {
+        // todo. add logic to construct from a Game
+        // note. client board can be obtained from previous information
+    }
 }
+
+
