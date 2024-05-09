9a81bedf075fc934f1caba270c9ddd13bf3c5772
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-09 23:16:00 +0200

    refactor: extend GeneralCard

--- src/main/java/it/polimi/ingsw/network/client/model/card/ClientCard.java
+++ src/main/java/it/polimi/ingsw/network/client/model/card/ClientCard.java
@@ -1,13 +1,15 @@
 package it.polimi.ingsw.network.client.model.card;
 
+import it.polimi.ingsw.GeneralCard;
 import it.polimi.ingsw.model.card.Symbol;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class ClientCard {
+public class ClientCard implements GeneralCard {
     ClientFace front;
     ClientFace back;
+    Map<Symbol, Integer> requirements; /* set? */
 
     public ClientCard() {
         front = new ClientFace();
@@ -19,16 +21,25 @@ public class ClientCard {
         back = new ClientFace(backID);
     }
 
+    // copy constructor
+    public ClientCard(GeneralCard other) {
+        this.front = new ClientFace(other.getFrontId());
+        this.back = new ClientFace(other.getBackId());
+        this.requirements = new HashMap<>(other.getRequiredResources());
+    }
+
+    @Override
     public int getBackId() {
         return back.getFaceID();
     }
 
+    @Override
     public int getFrontId() {
         return front.getFaceID();
     }
 
+    @Override
     public Map<Symbol, Integer> getRequiredResources() {
-        return new HashMap<>();
+        return requirements;
     }
-
 }
