06a20d9cecb72eb96d96cdbc4ed5d68111c8c6df
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-11 18:39:16 +0200

    refactor(ClientBoard): change the implementation of decks and face up cards

--- src/main/java/it/polimi/ingsw/network/client/model/board/ClientBoard.java
+++ src/main/java/it/polimi/ingsw/network/client/model/board/ClientBoard.java
@@ -1,59 +1,67 @@
 package it.polimi.ingsw.network.client.model.board;
 
+import it.polimi.ingsw.network.client.model.card.ClientCard;
+import it.polimi.ingsw.network.client.model.card.ClientFace;
+
 import java.util.ArrayList;
 
 
 public class ClientBoard {
 
-    private int[] faceUpCardsFrontID; //max 4
+    private ClientCard[] faceUpCards;
 
-    private int[] faceUpCardsBackID;
+    private ClientFace goldenDeckTopBack; //contains only backID numbers
 
-    private ArrayList<Integer> goldenDeck; //contains only backID numbers
-
-    private ArrayList<Integer> resourceDeck; //contains only backID numbers
+    private ClientFace resourceDeckTopBack; //contains only backID numbers
 
     private int[] commonObjectives;
 
     public ClientBoard() {
-        faceUpCardsFrontID = new int[4];
-        faceUpCardsBackID = new int[4];
+        faceUpCards = new ClientCard[4];
         commonObjectives = new int[2];
-        goldenDeck = new ArrayList<>();
-        resourceDeck = new ArrayList<>();
     }
 
     //getter methods
 
-    public int[] getFaceUpCards() {return faceUpCardsFrontID;}
 
-    public ArrayList<Integer> getGoldenDeck() {return goldenDeck;}
+    public ClientFace getGoldenDeckTopBack() {
+        return goldenDeckTopBack;
+    }
 
-    public ArrayList<Integer> getResourceDeck() {return resourceDeck;}
+    public ClientFace getResourceDeckTopBack() {
+        return resourceDeckTopBack;
+    }
+
+    public ClientCard[] getFaceUpCards() {
+        return faceUpCards;
+    }
 
     public int[] getCommonObjectives() {return commonObjectives;}
 
     //setter methods
 
+    public void setGoldenDeckTopBack(ClientFace goldenDeckTopBack) {
+        this.goldenDeckTopBack = goldenDeckTopBack;
+    }
+
+    public void setResourceDeckTopBack(ClientFace resourceDeckTopBack) {
+        this.resourceDeckTopBack = resourceDeckTopBack;
+    }
 
     public void setCommonObjectives(int[] commonObjectives) {
         this.commonObjectives = commonObjectives;
     }
 
-    public void setFaceUpCards(int[] faceUpCards) {
-        this.faceUpCardsFrontID = faceUpCards;
+    public void addFaceUpCards(ClientCard faceUpCard, int positionFaceUp) {
+        this.faceUpCards[positionFaceUp] = faceUpCard;
     }
 
-    public void addGoldenBackID(int goldenBackID) {
-        goldenDeck.add(goldenBackID);
+    public boolean isGoldenDeckEmpty(){
+        return goldenDeckTopBack == null;
     }
 
-    public void addResourceBackID(int resourceBackID) {
-        resourceDeck.add(resourceBackID);
+    public boolean isResourceDeckEmpty(){
+        return resourceDeckTopBack == null;
     }
 
-    public void addFaceUpCards(int newFrontFaceUp, int newBackFaceUp, int positionFaceUp) {
-        this.faceUpCardsFrontID[positionFaceUp] = newFrontFaceUp;
-        this.faceUpCardsBackID[positionFaceUp] = newBackFaceUp;
-    }
 }
