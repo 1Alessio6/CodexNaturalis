eaaf1b06f1a135f35b8d4ed94aef5968658169ed
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-11 15:27:51 +0200

    feat(ClientActions): add SuspendedGameException

--- src/main/java/it/polimi/ingsw/network/client/controller/ClientActions.java
+++ src/main/java/it/polimi/ingsw/network/client/controller/ClientActions.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.network.client.controller;
 
 import it.polimi.ingsw.model.InvalidGamePhaseException;
+import it.polimi.ingsw.model.SuspendedGameException;
 import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
 import it.polimi.ingsw.model.card.Color.InvalidColorException;
@@ -16,14 +17,14 @@ import it.polimi.ingsw.network.VirtualView;
 public interface ClientActions {
 
     void connect(VirtualView client, String username) throws InvalidUsernameException;
-    void placeCard(int cardHandPosition, Side selectedSide, Position position) throws Playground.UnavailablePositionException, Playground.NotEnoughResourcesException, InvalidGamePhaseException;
-    void draw(int IdToDraw) throws  InvalidGamePhaseException, EmptyDeckException, NotExistingFaceUp;
+    void placeCard(int cardHandPosition, Side selectedSide, Position position) throws Playground.UnavailablePositionException, Playground.NotEnoughResourcesException, InvalidGamePhaseException, SuspendedGameException;
+    void draw(int IdToDraw) throws  InvalidGamePhaseException, EmptyDeckException, NotExistingFaceUp, SuspendedGameException;
 
-    void placeStarter(Side side);
+    void placeStarter(Side side) throws SuspendedGameException;
 
-    void chooseColor(PlayerColor color) throws InvalidColorException;
+    void chooseColor(PlayerColor color) throws InvalidColorException, SuspendedGameException;
 
-    void placeObjectiveCard(int chosenObjective);
+    void placeObjectiveCard(int chosenObjective) throws SuspendedGameException;
 
     void sendMessage(Message message) throws InvalidMessageException;
 
