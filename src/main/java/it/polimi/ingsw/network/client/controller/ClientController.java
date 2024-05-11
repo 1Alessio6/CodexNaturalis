cdd93a6ab43b67ba6ae32c91e5f7f0b835f62931
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-11 15:32:42 +0200

    refactor(client): move placeCard server invocation from ClientRMI to ClientController

--- src/main/java/it/polimi/ingsw/network/client/controller/ClientController.java
+++ src/main/java/it/polimi/ingsw/network/client/controller/ClientController.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.network.client.controller;
 
 import it.polimi.ingsw.model.InvalidGamePhaseException;
+import it.polimi.ingsw.model.SuspendedGameException;
 import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
 import it.polimi.ingsw.model.card.*;
@@ -9,6 +10,7 @@ import it.polimi.ingsw.model.card.Color.PlayerColor;
 import it.polimi.ingsw.model.chat.message.InvalidMessageException;
 import it.polimi.ingsw.model.chat.message.Message;
 import it.polimi.ingsw.model.lobby.InvalidUsernameException;
+import it.polimi.ingsw.model.player.InvalidPlayerActionException;
 import it.polimi.ingsw.network.VirtualServer;
 import it.polimi.ingsw.network.VirtualView;
 import it.polimi.ingsw.network.client.model.ClientGame;
@@ -20,7 +22,7 @@ import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
 
-public class ClientController implements ClientActions{
+public class ClientController implements ClientActions {
 
     private VirtualServer server;
     private ClientGame game;
@@ -31,27 +33,48 @@ public class ClientController implements ClientActions{
     }
 
     @Override
-    public void placeCard(int cardHandPosition, Side selectedSide, Position position) throws Playground.UnavailablePositionException, Playground.NotEnoughResourcesException, InvalidGamePhaseException {
+    public void placeCard(int cardHandPosition, Side selectedSide, Position position) throws Playground.UnavailablePositionException, Playground.NotEnoughResourcesException, InvalidGamePhaseException, SuspendedGameException {
+
+        if (!game.isGameActive()){
+            throw new SuspendedGameException("The game is suspended, you can only text");
+        }
+
+        if (selectedSide == Side.FRONT) {
+            if (!checkRequirements(this.game.getMainPlayerCard(cardHandPosition))) {
+                throw new Playground.NotEnoughResourcesException("You don't have the resources to place this side of the card");
+            }
+        }
+
+        if (!checkPosition(position)) {
+            throw new Playground.UnavailablePositionException("The position insert isn't available");
+        }
+
+        try{
+            server.placeCard(this.game.getMainPlayerUsername(), this.game.getMainPlayerCard(cardHandPosition).getBackId(), this.game.getMainPlayerCard(cardHandPosition).getFrontId(), selectedSide, position);
+        } catch (Playground.UnavailablePositionException | Playground.NotEnoughResourcesException e) {
+            System.err.println("Error check methods should have avoid an incorrect move");
+        } catch (InvalidPlayerActionException | SuspendedGameException | InvalidGamePhaseException e) {
+            System.err.println("Error");
+        }
+    }
+
+    @Override
+    public void draw(int IdToDraw) throws InvalidGamePhaseException, EmptyDeckException, NotExistingFaceUp, SuspendedGameException {
 
     }
 
     @Override
-    public void draw(int IdToDraw) throws InvalidGamePhaseException, EmptyDeckException, NotExistingFaceUp {
+    public void placeStarter(Side side) throws SuspendedGameException{
 
     }
 
     @Override
-    public void placeStarter(Side side){
+    public void chooseColor(PlayerColor color) throws InvalidColorException, SuspendedGameException {
 
     }
 
     @Override
-    public void chooseColor(PlayerColor color) throws InvalidColorException{
-
-    }
-
-    @Override
-    public void placeObjectiveCard(int chosenObjective){
+    public void placeObjectiveCard(int chosenObjective) throws SuspendedGameException{
 
     }
 
@@ -81,27 +104,27 @@ public class ClientController implements ClientActions{
 
     }
 
-    public void updateInitialPlayerStatus(ClientPlayer player){
+    public void updateInitialPlayerStatus(ClientPlayer player) {
 
     }
 
-    public void updateBoardSetUp(int[] commonObjectiveID, int topBackID, int topGoldenBackID, int[] faceUpCards){
+    public void updateBoardSetUp(int[] commonObjectiveID, int topBackID, int topGoldenBackID, int[] faceUpCards) {
 
     }
 
-    public void updateStarterPlacement(String username, int faceId){
+    public void updateStarterPlacement(String username, int faceId) {
 
     }
 
-    public void updateColor(PlayerColor color, String username){
+    public void updateColor(PlayerColor color, String username) {
 
     }
 
-    public void updateObjectiveCard(ClientCard chosenObjective, String username){
+    public void updateObjectiveCard(ClientCard chosenObjective, String username) {
 
     }
 
-    void updateAfterPlace(Map<Position, CornerPosition> positionToCornerCovered, List<Position> newAvailablePositions, Map<Symbol, Integer> newResources, int points, String username, ClientCard placedCard , Position position){
+    void updateAfterPlace(Map<Position, CornerPosition> positionToCornerCovered, List<Position> newAvailablePositions, Map<Symbol, Integer> newResources, int points, String username, ClientCard placedCard, Position position) {
 
     }
 
@@ -111,19 +134,32 @@ public class ClientController implements ClientActions{
 
     //method to notify update after a draw
 
-    void updateChat(Message message){
+    void updateChat(Message message) {
 
     }
 
-    void updateCurrentPlayer(ClientPlayer currentPlayer, ClientPhase phase){
+    void updateCurrentPlayer(ClientPlayer currentPlayer, ClientPhase phase) {
 
     }
 
-    void updateSuspendedGame(){
+    void updateSuspendedGame() {
 
     }
 
-    void showWinners(List<String> winners){
+    void showWinners(List<String> winners) {
+    }
+
+    private boolean checkPosition(Position position) {
+        return game.getMainPlayerPlayground().getAvailablePositions().contains(position);
+    }
+
+    private boolean checkRequirements(ClientCard card) {
+        for (Symbol s : card.getRequiredResources().keySet()) {
+            if (this.game.getMainPlayer().getAmountResource(s) < card.getRequiredResources().get(s)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
