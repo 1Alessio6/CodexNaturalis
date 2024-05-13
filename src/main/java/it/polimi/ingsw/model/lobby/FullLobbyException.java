5bd9fd9da868875e68bb5e423572f6ac7ef5395b
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-05-13 13:51:55 +0200

    feat: set default message for exceptions

--- src/main/java/it/polimi/ingsw/model/lobby/FullLobbyException.java
+++ src/main/java/it/polimi/ingsw/model/lobby/FullLobbyException.java
@@ -1,9 +1,7 @@
 package it.polimi.ingsw.model.lobby;
 
 public class FullLobbyException extends Exception {
-    public FullLobbyException() {}
-
-    public FullLobbyException(String message) {
-        super(message);
+    public FullLobbyException() {
+        super("Game hasn't started yet, but lobby is already full");
     }
 }
