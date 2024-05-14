fbfd2d58ae14b59d92c01d9250f779e26669f5c2
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-14 16:20:40 +0200

    feat(ClientHandler): update run method

--- src/main/java/it/polimi/ingsw/network/client/socket/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/ClientHandler.java
@@ -1,21 +1,29 @@
 package it.polimi.ingsw.network.client.socket;
 
 import com.google.gson.Gson;
+import it.polimi.ingsw.controller.InvalidIdForDrawingException;
+import it.polimi.ingsw.model.InvalidGamePhaseException;
+import it.polimi.ingsw.model.NonexistentPlayerException;
+import it.polimi.ingsw.model.SuspendedGameException;
+import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
+import it.polimi.ingsw.model.card.*;
+import it.polimi.ingsw.model.card.Color.InvalidColorException;
 import it.polimi.ingsw.model.card.Color.PlayerColor;
-import it.polimi.ingsw.model.card.CornerPosition;
-import it.polimi.ingsw.model.card.Side;
-import it.polimi.ingsw.model.card.Symbol;
+import it.polimi.ingsw.model.chat.message.InvalidMessageException;
 import it.polimi.ingsw.model.chat.message.Message;
 import it.polimi.ingsw.model.gamePhase.GamePhase;
+import it.polimi.ingsw.model.lobby.FullLobbyException;
+import it.polimi.ingsw.model.lobby.InvalidPlayersNumberException;
+import it.polimi.ingsw.model.lobby.InvalidUsernameException;
+import it.polimi.ingsw.model.player.InvalidPlayerActionException;
 import it.polimi.ingsw.network.VirtualView;
 import it.polimi.ingsw.network.client.model.ClientGame;
 import it.polimi.ingsw.network.client.model.card.ClientCard;
 import it.polimi.ingsw.network.client.model.card.ClientFace;
 import it.polimi.ingsw.network.client.socket.message.*;
 import it.polimi.ingsw.network.server.socket.Server;
-import it.polimi.ingsw.network.server.socket.message.ServerMessage;
-import it.polimi.ingsw.network.server.socket.message.ServerType;
+import it.polimi.ingsw.network.server.socket.message.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -41,46 +49,110 @@ public class ClientHandler implements VirtualView {
 
     public void run() throws IOException {
         String line = input.readLine();
-        while(line != null){
+        while (line != null) {
             ServerMessage message = gson.fromJson(line, ServerMessage.class);
             ServerType type = message.getType();
+            String sender = message.getSender();
 
-            switch (type){
+
+            switch (type) {
                 case CONNECT:
+                    assert message instanceof ConnectMessage;
+                    ConnectMessage connectMessage = (ConnectMessage)message;
+
+                    try {
+                        server.connect(connectMessage.getClient(),sender);
+                    } catch (FullLobbyException | InvalidUsernameException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case PLACE_STARTER:
+                    assert message instanceof  PlaceStarterMessage;
+                    PlaceStarterMessage placeStarterMessage = (PlaceStarterMessage) message;
+                    try {
+                        server.placeStarter(sender, placeStarterMessage.getSide());
+                    } catch (InvalidPlayerActionException | InvalidGamePhaseException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case CHOOSE_COLOR:
+                    assert message instanceof ChooseColorMessage;
+                    ChooseColorMessage chooseColorMessage = (ChooseColorMessage) message;
+
+                    try {
+                        server.chooseColor(sender, chooseColorMessage.getColor());
+                    } catch (NonexistentPlayerException | InvalidGamePhaseException | InvalidPlayerActionException |
+                             InvalidColorException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case PLACE_OBJECTIVE:
+                    assert message instanceof PlaceObjectiveMessage;
+                    PlaceObjectiveMessage placeObjectiveMessage = (PlaceObjectiveMessage) message;
+
+                    try {
+                        server.placeObjectiveCard(sender,placeObjectiveMessage.getChosenObjective());
+                    } catch (InvalidPlayerActionException | InvalidGamePhaseException e) {
+                        throw new RuntimeException(e);
+                    }
 
                 case PLACE_CARD:
+                    assert message instanceof PlaceCardMessage;
+                    PlaceCardMessage placeCardMessage = (PlaceCardMessage) message;
+                    try {
+                        server.placeCard(sender, placeCardMessage.getFrontId(), placeCardMessage.getBackId(), placeCardMessage.getSide(), placeCardMessage.getPosition());
+                    } catch (InvalidPlayerActionException | Playground.UnavailablePositionException |
+                             Playground.NotEnoughResourcesException | InvalidGamePhaseException |
+                             SuspendedGameException | InvalidCardIdException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case DRAW:
+                    assert message instanceof DrawMessage;
+                    DrawMessage drawMessage = (DrawMessage) message;
+
+                    try {
+                        server.draw(sender, drawMessage.getIdDraw());
+                    } catch (InvalidGamePhaseException | InvalidPlayerActionException | EmptyDeckException |
+                             InvalidIdForDrawingException | InvalidFaceUpCardException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case SEND_CHAT_MESSAGE:
+                    assert message instanceof SendChatMessage;
+                    SendChatMessage sendChatMessage = (SendChatMessage) message;
+
+                    try {
+                        server.sendMessage(sendChatMessage.getMessage());
+                    } catch (InvalidMessageException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case SET_PLAYER_NUMBER:
+                    assert message instanceof SetPlayerNumberMessage;
+                    SetPlayerNumberMessage setPlayerNumberMessage = (SetPlayerNumberMessage) message;
+
+                    try {
+                        server.setPlayersNumber(sender, setPlayerNumberMessage.getNumPlayers());
+                    } catch (InvalidPlayersNumberException e) {
+                        System.err.println(e.getMessage());
+                    }
 
                 case DISCONNECT:
+                    try {
+                        server.disconnect(sender);
+                    } catch (RemoteException | InvalidUsernameException e) {
+                        System.err.println(e.getMessage());
+                    }
+
 
                 case SEND_PING:
-
+                    server.sendPing(sender);
             }
-
 
             line = input.readLine();
         }
     }
-
-    /*
-
-    SEND_CHAT_MESSAGE,
-    SET_PLAYER_NUMBER,
-    DISCONNECT,
-    SEND_PING
-     */
-
 
     @Override
     public void updateCreator() throws RemoteException {
@@ -116,7 +188,7 @@ public class ClientHandler implements VirtualView {
 
     @Override
     public void showUpdatePlayerStatus(boolean isConnected, String username) throws RemoteException {
-        UpdatePlayerStatusMessage message = new UpdatePlayerStatusMessage(isConnected,username);
+        UpdatePlayerStatusMessage message = new UpdatePlayerStatusMessage(isConnected, username);
         String jsonMessage = gson.toJson(message);
         out.println(jsonMessage);
         out.flush();
@@ -124,7 +196,7 @@ public class ClientHandler implements VirtualView {
 
     @Override
     public void showUpdateColor(PlayerColor color, String username) throws RemoteException {
-        UpdateColorMessage message = new UpdateColorMessage(username,color);
+        UpdateColorMessage message = new UpdateColorMessage(username, color);
         String jsonMessage = gson.toJson(message);
         out.println(jsonMessage);
         out.flush();
