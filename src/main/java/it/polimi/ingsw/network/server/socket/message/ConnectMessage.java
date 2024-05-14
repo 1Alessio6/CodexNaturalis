1eb1b421d67cd85212caef827b244efb4b2af415
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-14 21:38:13 +0200

    refactor(ConnectMessage): remove VirtualView from the parameter

--- src/main/java/it/polimi/ingsw/network/server/socket/message/ConnectMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/ConnectMessage.java
@@ -1,16 +1,7 @@
 package it.polimi.ingsw.network.server.socket.message;
 
-import it.polimi.ingsw.network.VirtualView;
-
 public class ConnectMessage extends ServerMessage {
-
-    private final VirtualView client;
-    public ConnectMessage(VirtualView client, String sender) {
+    public ConnectMessage(String sender) {
         super(sender, ServerType.CONNECT);
-        this.client = client;
-    }
-
-    public VirtualView getClient() {
-        return client;
     }
 }
