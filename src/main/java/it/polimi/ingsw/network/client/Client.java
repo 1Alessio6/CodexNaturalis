5c59d3c34f822c35c9e5d4d8148f6cc26de972b4
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-20 17:03:11 +0200

    feat: add class Client

---
+++ src/main/java/it/polimi/ingsw/network/client/Client.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.network.client;
+
+import it.polimi.ingsw.network.VirtualServer;
+import it.polimi.ingsw.network.client.controller.ClientController;
+import it.polimi.ingsw.network.client.view.View;
+import it.polimi.ingsw.network.heartbeat.PingReceiver;
+
+import java.rmi.RemoteException;
+
+public abstract class Client implements PingReceiver {
+    protected ClientController controller;
+    protected View clientView;
+    protected VirtualServer server;
+
+    protected abstract VirtualServer connect(String ip, Integer port);
+
+    public Client(String ip, int port) throws UnReachableServerException {
+        server = connect(ip, port);
+        if (server == null) {
+            throw new UnReachableServerException();
+        }
+        this.controller = new ClientController(server);
+    }
+
+    public void addView(View view) {
+        clientView = view;
+    }
+
+    public ClientController getController() {
+        return controller;
+    }
+
+    public abstract void runView() throws RemoteException;
+}
