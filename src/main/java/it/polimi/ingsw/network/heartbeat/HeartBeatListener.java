e991a75bd8cd80c323487b2308126ac9dfadc6ce
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-21 12:07:47 +0200

    fix(HeartBeatListener): extend Remote

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatListener.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatListener.java
@@ -1,7 +1,8 @@
 package it.polimi.ingsw.network.heartbeat;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface HeartBeatListener {
+public interface HeartBeatListener extends Remote {
     void receivePing(String senderName) throws RemoteException;
 }
