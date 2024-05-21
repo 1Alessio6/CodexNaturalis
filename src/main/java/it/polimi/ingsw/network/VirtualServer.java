30d80b5a4e6a23b9ec9923c98b5b7f426fe9625d
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-21 09:07:42 +0200

    refactor: rename PingSender to HeartBeatListener

--- src/main/java/it/polimi/ingsw/network/VirtualServer.java
+++ src/main/java/it/polimi/ingsw/network/VirtualServer.java
@@ -1,32 +1,16 @@
 package it.polimi.ingsw.network;
 
-import it.polimi.ingsw.controller.GameRequest;
-import it.polimi.ingsw.controller.InvalidIdForDrawingException;
-import it.polimi.ingsw.model.InvalidGamePhaseException;
-import it.polimi.ingsw.model.NonexistentPlayerException;
-import it.polimi.ingsw.model.SuspendedGameException;
-import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
-import it.polimi.ingsw.model.card.Color.InvalidColorException;
 import it.polimi.ingsw.model.card.Color.PlayerColor;
-import it.polimi.ingsw.model.card.EmptyDeckException;
-import it.polimi.ingsw.model.card.InvalidCardIdException;
-import it.polimi.ingsw.model.card.InvalidFaceUpCardException;
 import it.polimi.ingsw.model.card.Side;
-import it.polimi.ingsw.model.chat.message.InvalidMessageException;
 import it.polimi.ingsw.model.chat.message.Message;
-import it.polimi.ingsw.model.lobby.FullLobbyException;
-import it.polimi.ingsw.model.lobby.InvalidPlayersNumberException;
-import it.polimi.ingsw.model.lobby.InvalidUsernameException;
-import it.polimi.ingsw.model.player.InvalidPlayerActionException;
-import it.polimi.ingsw.model.player.NotAvailableUsername;
-import it.polimi.ingsw.network.heartbeat.PingSender;
+import it.polimi.ingsw.network.heartbeat.HeartBeatListener;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 
-public interface VirtualServer extends Remote, PingSender {
+public interface VirtualServer extends Remote, HeartBeatListener {
     /**
      * Connect to server
      *
