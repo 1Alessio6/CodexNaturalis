23854c4599e17827e9662eb43a8b8f89660c455d
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-22 16:12:44 +0200

    feat(gui): add SceneController abstract class

---
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/SceneController.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.network.client.view.gui.controllers;
+
+import it.polimi.ingsw.network.client.view.gui.ClientGUI;
+
+public abstract class SceneController {
+    protected ClientGUI gui;
+
+    public void setGui(ClientGUI gui) {
+        this.gui = gui;
+    }
+
+}
