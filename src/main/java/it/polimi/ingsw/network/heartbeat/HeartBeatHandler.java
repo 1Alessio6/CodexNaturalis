8399a7638eda1d72b05178ad123b651b910e15f2
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-22 15:57:28 +0200

    refactor: refactor HeartBeatHandler  and HeartBeatListener

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatHandler.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatHandler.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.network.heartbeat;
 
-public interface HeartBeatHandler {
-    void handleUnresponsiveness();
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface HeartBeatHandler extends Remote {
+    void handleUnresponsiveness(String unresponsiveListener) throws RemoteException;
 }
--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatListener.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatListener.java
@@ -3,6 +3,6 @@ package it.polimi.ingsw.network.heartbeat;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface HeartBeatListener extends Remote {
-    void receivePing(String senderName) throws RemoteException;
+public interface HeartBeatListener extends Remote{
+    void receivePing(HeartBeatMessage ping) throws RemoteException;
 }
