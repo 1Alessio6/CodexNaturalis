5331bc3713b0456053d88229ef120d433d9286c9
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-05-22 16:05:15 +0200

    refactor: delete unused files

--- src/main/java/it/polimi/ingsw/network/server/socket/message/ServerType.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/ServerType.java
@@ -1,14 +1,14 @@
 package it.polimi.ingsw.network.server.socket.message;
 
-public enum ServerType {
-    CONNECT,
-    PLACE_STARTER,
-    CHOOSE_COLOR,
-    PLACE_OBJECTIVE,
-    PLACE_CARD,
-    DRAW,
-    SEND_CHAT_MESSAGE,
-    SET_PLAYER_NUMBER,
-    DISCONNECT,
-    SEND_PING
-}
+//public enum ServerType {
+//    CONNECT,
+//    PLACE_STARTER,
+//    CHOOSE_COLOR,
+//    PLACE_OBJECTIVE,
+//    PLACE_CARD,
+//    DRAW,
+//    SEND_CHAT_MESSAGE,
+//    SET_PLAYER_NUMBER,
+//    DISCONNECT,
+//    SEND_PING
+//}
