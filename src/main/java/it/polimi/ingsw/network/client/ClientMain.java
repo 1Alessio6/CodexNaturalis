376191b4458bf53fe9308e0ae3a296701c19f06c
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-05-24 18:11:47 +0200

    feat(ClientMain): add cli argument parser

--- src/main/java/it/polimi/ingsw/network/client/ClientMain.java
+++ src/main/java/it/polimi/ingsw/network/client/ClientMain.java
@@ -5,8 +5,11 @@ import it.polimi.ingsw.network.client.socket.ClientSocket;
 import it.polimi.ingsw.network.client.view.View;
 import it.polimi.ingsw.network.client.view.gui.ClientGUI;
 import it.polimi.ingsw.network.client.view.tui.ClientTUI;
+import it.polimi.ingsw.network.client.view.tui.ClientUtil;
 
 import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class ClientMain {
     private static final String OPTION_RMI = "rmi";
@@ -32,16 +35,39 @@ public class ClientMain {
     }
 
     public static void main(String[] args) {
-        String typeConnection = OPTION_RMI;
+        String typeConnection = OPTION_SOCKET;
         String typeView = OPTION_TUI;
+        String host = "localhost";
+        int port = 1234;
 
         // todo(command-line parser)
-
         try {
-            Client c = createClient(args[0], Integer.parseInt(args[1]), typeConnection, typeView);
+            // max 4 args: rmi tui port <int>
+            if (args.length > 0 && args.length <= 6) {
+                // override stuff
+                if (Arrays.stream(args).anyMatch("--rmi"::equalsIgnoreCase)) {
+                    typeConnection = OPTION_RMI;
+                }
+                if (Arrays.stream(args).anyMatch("--tui"::equalsIgnoreCase)) {
+                    typeView = OPTION_TUI;
+                }
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i].equalsIgnoreCase("--port")) {
+                        port = Integer.parseInt(args[i+1]);
+                    }
+
+                    if (args[i].equalsIgnoreCase("--host")) {
+                        host = args[i+1];
+                    }
+                }
+            } else if (args.length > 6){
+                ClientUtil.argsHelper("Too many arguments");
+            }
+
+            Client c = createClient(host, port, typeConnection, typeView);
             c.runView();
-        } catch (UnReachableServerException | RemoteException e) {
-            System.err.println("Error: " + e.getMessage());
+        } catch (UnReachableServerException | RemoteException | IndexOutOfBoundsException e) {
+            ClientUtil.argsHelper(e.getMessage());
             System.exit(1);
         }
     }
