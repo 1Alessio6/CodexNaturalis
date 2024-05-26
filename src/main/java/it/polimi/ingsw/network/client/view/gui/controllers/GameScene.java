efde906dba0527257430a968314359d85c63504d
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-26 13:28:13 +0200

    feat: update GameActiveScene.fxml and related controller class GameActiveScene

--- src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/GameScene.java
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/GameScene.java
@@ -1,8 +1,26 @@
 package it.polimi.ingsw.network.client.view.gui.controllers;
 
+import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.gamePhase.GamePhase;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
-public class GameScene {
+public class GameScene extends SceneController{
+
+    @FXML
+    private Pane playgroundPane;
+
+    private Pane chat;
+
+    private Pane firstPlayerPane;
+    private Pane secondPlayerPane;
+    private Pane thirdPlayerPane;
+
+    private Pane firstPlayerCards;
+    private Pane secondPlayerCards;
+    private Pane thirdPlayerCards;
+
 
     public GameScene(){}
+
 }
