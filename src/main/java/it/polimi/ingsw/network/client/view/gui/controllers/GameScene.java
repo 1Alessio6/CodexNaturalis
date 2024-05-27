6bf261f332cc0fcdf8d6c161ba0ffbfd7098fdb8
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-27 23:41:35 +0200

    feat(GameScene): update in order to get place command information through available tiles

--- src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/GameScene.java
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/GameScene.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.network.client.view.gui.controllers;
 
+import it.polimi.ingsw.model.InvalidGamePhaseException;
+import it.polimi.ingsw.model.SuspendedGameException;
+import it.polimi.ingsw.model.board.Playground;
 import it.polimi.ingsw.model.board.Position;
 import it.polimi.ingsw.model.card.Side;
 import it.polimi.ingsw.network.client.model.board.ClientPlayground;
@@ -12,6 +15,7 @@ import it.polimi.ingsw.network.client.view.gui.util.PlayerInfoPane;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -19,8 +23,11 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.awt.image.ImageObserver.HEIGHT;
 import static java.awt.image.ImageObserver.WIDTH;
@@ -38,9 +45,9 @@ public class GameScene extends SceneController {
     private Pane chat;
 
     private List<Side> playerCardsVisibleSide;
+
+    private List<Rectangle> availablePositions;
     private int selectedCardHandPosition;
-
-
 
 
     public GameScene() {
@@ -49,6 +56,7 @@ public class GameScene extends SceneController {
     public void initialize() {
         initializePlayerInfoBox();
         playerCardsVisibleSide = new ArrayList<>();
+        availablePositions = new ArrayList<>();
         selectedCardHandPosition = -1;
 
     }
@@ -88,24 +96,26 @@ public class GameScene extends SceneController {
             ImagePattern backImage = new ImagePattern(new Image(cards.get(i).getBackPath()));
             ImagePattern frontImage = new ImagePattern(new Image(cards.get(i).getFrontPath()));
             rectangle.setFill(frontImage);
-            playerCardsVisibleSide.add(i,Side.FRONT);
+            playerCardsVisibleSide.add(i, Side.FRONT);
             rectangle.setLayoutX(layoutX);
             layoutX = layoutX + 151 + 30;
 
             rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-                    if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED && mouseEvent.getButton() == MouseButton.SECONDARY) {
+                    if (isClicked(mouseEvent, MouseButton.SECONDARY)) {
                         if (rectangle.getFill() == frontImage) {
                             rectangle.setFill(backImage);
-                            playerCardsVisibleSide.set(cardHandPosition,Side.BACK);
+                            playerCardsVisibleSide.set(cardHandPosition, Side.BACK);
                             return;
                         }
                         rectangle.setFill(frontImage);
-                        playerCardsVisibleSide.set(cardHandPosition,Side.FRONT);
+                        playerCardsVisibleSide.set(cardHandPosition, Side.FRONT);
 
-                    }
-                    else if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED && mouseEvent.getButton() == MouseButton.PRIMARY){
+                    } else if (isClicked(mouseEvent, MouseButton.PRIMARY)) {
+                        for (Rectangle availableTile : availablePositions) {
+                            availableTile.setVisible(true);
+                        }
                         selectCard(cardHandPosition);
                     }
                 }
@@ -117,20 +127,20 @@ public class GameScene extends SceneController {
 
     }
 
-    private void selectCard(int cardHandPosition){
-        if(selectedCardHandPosition == -1){
+    private void selectCard(int cardHandPosition) {
+        if (selectedCardHandPosition == -1) {
             selectedCardHandPosition = cardHandPosition;
-        }
-        else if(selectedCardHandPosition == cardHandPosition){
+        } else if (selectedCardHandPosition == cardHandPosition) {
             selectedCardHandPosition = -1;
-        }
-        else{
+        } else {
             selectedCardHandPosition = cardHandPosition;
         }
     }
 
     public void drawPlayground(ClientPlayground clientPlayground) {
 
+        //do not remove
+        availablePositions.clear();
 
         GUIPlayground guiPlayground = new GUIPlayground(WIDTH, HEIGHT);
         guiPlayground.setDimension(clientPlayground);
@@ -155,10 +165,40 @@ public class GameScene extends SceneController {
 
         for (Position pos : clientPlayground.getAvailablePositions()) {
             Rectangle rectangle = guiPlayground.getRectangleEmptyTile(pos);
+            rectangle.setVisible(false);
+            rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+
+                    if (isClicked(mouseEvent, MouseButton.PRIMARY) && selectedCardHandPosition != -1) {
+
+                        try{
+                            gui.getController().placeCard(selectedCardHandPosition, playerCardsVisibleSide.get(selectedCardHandPosition), pos);
+                        }catch (Playground.UnavailablePositionException | InvalidGamePhaseException | SuspendedGameException | Playground.NotEnoughResourcesException | RemoteException e){
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setTitle(e.getMessage());
+                            alert.setContentText(e.getMessage());
+                            alert.show();
+                        }
+
+                        selectedCardHandPosition = -1;
+
+                        for (Rectangle availableTile : availablePositions) {
+                            availableTile.setVisible(false);
+                        }
+                    }
+                }
+
+            });
+            availablePositions.add(rectangle);
             playgroundPane.getChildren().add(rectangle);
         }
 
 
+    }
+
+    private boolean isClicked(MouseEvent mouseEvent, MouseButton mouseButton) {
+        return mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED && mouseEvent.getButton() == mouseButton;
     }
 
 
