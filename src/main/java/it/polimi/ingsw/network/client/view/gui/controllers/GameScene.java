6b472036e0f2b73c5a977054f3d1078e4f89ad96
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-27 23:19:29 +0200

    feat(GUICards): edit the class to gather required details for choosing a card from the hand.

--- src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/GameScene.java
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/GameScene.java
@@ -1,18 +1,29 @@
 package it.polimi.ingsw.network.client.view.gui.controllers;
 
+import it.polimi.ingsw.model.board.Position;
 import it.polimi.ingsw.model.card.Side;
+import it.polimi.ingsw.network.client.model.board.ClientPlayground;
 import it.polimi.ingsw.network.client.model.card.ClientCard;
+import it.polimi.ingsw.network.client.model.card.ClientFace;
 import it.polimi.ingsw.network.client.model.player.ClientPlayer;
+import it.polimi.ingsw.network.client.view.gui.GUIPlayground;
+import it.polimi.ingsw.network.client.view.gui.util.GUICards;
 import it.polimi.ingsw.network.client.view.gui.util.PlayerInfoPane;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.awt.image.ImageObserver.WIDTH;
 
 public class GameScene extends SceneController {
 
@@ -22,52 +33,23 @@ public class GameScene extends SceneController {
     @FXML
     private Pane playgroundPane;
 
+    private Pane mainPlayerCardsPane;
+
     private Pane chat;
 
-    @FXML
-    private void mainPlayerCards(){
+    private List<Side> playerCardsVisibleSide;
+    private int selectedCardHandPosition;
 
-    }
+
 
 
     public GameScene() {
     }
 
-
-    //todo modify with usernames
-    public static void initializePlayerCards(Pane playerPane, List<ClientCard> cards, int cardWidth, int cardHeight, int distanceBetweenCards) {
-
-        double layoutX = 0.0;
-
-        for (int i = 0; i < 3; i++) {
-            Rectangle rectangle = new Rectangle(cardWidth, cardHeight);
-            ImagePattern backImage = new ImagePattern(new Image(cards.get(i).getBackPath()));
-            ImagePattern frontImage = new ImagePattern(new Image(cards.get(i).getFrontPath()));
-            rectangle.setFill(frontImage);
-            rectangle.setLayoutX(layoutX);
-
-            rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    if(rectangle.getFill() == frontImage){
-                        rectangle.setFill(backImage);
-                        return;
-                    }
-                    rectangle.setFill(frontImage);
-                }
-            });
-
-            playerPane.getChildren().add(rectangle);
-            layoutX = layoutX + cardWidth + distanceBetweenCards;
-        }
-
-    }
-
     public void initialize() {
-
-
-
         initializePlayerInfoBox();
+        playerCardsVisibleSide = new ArrayList<>();
+        selectedCardHandPosition = -1;
 
     }
 
@@ -87,9 +69,96 @@ public class GameScene extends SceneController {
         }
     }
 
-    private ImagePattern getFacePath(String username, int cardHandPosition, Side side){
+    private ImagePattern getFacePath(String username, int cardHandPosition, Side side) {
         String path = gui.getController().getPlayer(username).getPlayerCard(cardHandPosition).getSidePath(side);
         return new ImagePattern(new Image(path));
+    }
+
+    //todo needs to be called after every place in order to have the correct association between cards and images
+    private void initializeMainPlayerCards() {
+        GUICards.initializePlayerCards(mainPlayerCardsPane, gui.getController().getMainPlayerCards(), 151, 98, 20, MouseButton.SECONDARY);
+
+        double layoutX = 0.0;
+        List<ClientCard> cards = gui.getController().getMainPlayerCards();
+
+        for (int i = 0; i < gui.getController().getMainPlayerCards().size(); i++) {
+
+            int cardHandPosition = i;
+            Rectangle rectangle = new Rectangle(151, 98);
+            ImagePattern backImage = new ImagePattern(new Image(cards.get(i).getBackPath()));
+            ImagePattern frontImage = new ImagePattern(new Image(cards.get(i).getFrontPath()));
+            rectangle.setFill(frontImage);
+            playerCardsVisibleSide.add(i,Side.FRONT);
+            rectangle.setLayoutX(layoutX);
+            layoutX = layoutX + 151 + 30;
+
+            rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED && mouseEvent.getButton() == MouseButton.SECONDARY) {
+                        if (rectangle.getFill() == frontImage) {
+                            rectangle.setFill(backImage);
+                            playerCardsVisibleSide.set(cardHandPosition,Side.BACK);
+                            return;
+                        }
+                        rectangle.setFill(frontImage);
+                        playerCardsVisibleSide.set(cardHandPosition,Side.FRONT);
+
+                    }
+                    else if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED && mouseEvent.getButton() == MouseButton.PRIMARY){
+                        selectCard(cardHandPosition);
+                    }
+                }
+
+            });
+
+            mainPlayerCardsPane.getChildren().add(rectangle);
+        }
+
+    }
+
+    private void selectCard(int cardHandPosition){
+        if(selectedCardHandPosition == -1){
+            selectedCardHandPosition = cardHandPosition;
+        }
+        else if(selectedCardHandPosition == cardHandPosition){
+            selectedCardHandPosition = -1;
+        }
+        else{
+            selectedCardHandPosition = cardHandPosition;
+        }
+    }
+
+    public void drawPlayground(ClientPlayground clientPlayground) {
+
+
+        GUIPlayground guiPlayground = new GUIPlayground(WIDTH, HEIGHT);
+        guiPlayground.setDimension(clientPlayground);
+        //   playgroundPane.setPrefSize(guiPlayground.getPaneWidth(), guiPlayground.getPaneHeight());
+        List<Rectangle> cardsAsRectangles = new ArrayList<>();
+        // todo. code just to run the playground. It must be inserted in the loop
+        Image img = null;
+        try {
+            img = new Image(
+                    "gui/png/cards/010.png"
+            );
+        } catch (Exception e) {
+            System.err.println("Image not found");
+            System.exit(1);
+        }
+
+        //it's necessary to add available position after the occupied one
+
+        for (Position pos : clientPlayground.getPositioningOrder()) {
+            playgroundPane.getChildren().add(guiPlayground.getRectangle(pos, img));
+        }
+
+        for (Position pos : clientPlayground.getAvailablePositions()) {
+            Rectangle rectangle = guiPlayground.getRectangleEmptyTile(pos);
+            playgroundPane.getChildren().add(rectangle);
+        }
+
+
     }
 
 
