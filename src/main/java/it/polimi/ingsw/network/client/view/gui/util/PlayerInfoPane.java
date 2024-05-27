859bc544a5ececb671024d3b90c9080f6a64420f
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-27 16:04:42 +0200

    feat(gui): add PlayerInfoPane class

---
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/util/PlayerInfoPane.java
@@ -0,0 +1,130 @@
+package it.polimi.ingsw.network.client.view.gui.util;
+
+import it.polimi.ingsw.model.card.Color.PlayerColor;
+import it.polimi.ingsw.network.client.model.player.ClientPlayer;
+import it.polimi.ingsw.network.client.view.gui.controllers.GameScene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class PlayerInfoPane {
+
+    private Pane playerMainPane;
+
+    private static final int mainPaneWidth = 436;
+
+    private static final int mainPaneHeight = 162;
+
+    private Pane playerCardsPane;
+
+    private static final int cardsPaneWidth = 275;
+
+    private static final int cardsPaneHeight = 48;
+
+    private Pane resourcesPane;
+
+    private static final int resourcesPaneWidth = 406;
+
+    private static final int resourcesPaneHeight = 37;
+
+    private Text username;
+
+    private Button switchPlayground;
+
+    private static final int switchPlaygroundWidth = 111;
+
+    private static final int switchPlaygroundHeight = 48;
+
+    private static final int distance = 25;
+
+    private static final int cardWidth = 74;
+
+    private static final int cardHeight = 48;
+
+
+
+    /*
+    <Pane layoutX="107.0" layoutY="14.0" prefHeight="162.0" prefWidth="436.0">
+         <children>
+            <Pane layoutX="15.0" layoutY="99.0" prefHeight="48.0" prefWidth="275.0" />
+            <Text layoutX="14.0" layoutY="26.0" strokeType="OUTSIDE" strokeWidth="0.0" text="PlayerName" wrappingWidth="385.13671875" />
+            <Pane layoutX="15.0" layoutY="51.0" prefHeight="37.0" prefWidth="406.0" />
+            <Button layoutX="317.0" layoutY="99.0" mnemonicParsing="false" prefHeight="48.0" prefWidth="111.0" text="PLAYGROUND" textAlignment="CENTER" />
+         </children></Pane>
+     */
+
+
+    public PlayerInfoPane(ClientPlayer player) {
+        playerMainPane = new Pane();
+        playerMainPane.setBackground(new Background(new BackgroundFill(Color.SADDLEBROWN, CornerRadii.EMPTY, null)));
+        playerMainPane.setPrefSize(mainPaneWidth, mainPaneHeight);
+
+        //initialize card pane
+        playerCardsPane = new Pane();
+        playerCardsPane.setPrefSize(cardsPaneWidth, cardsPaneHeight);
+        GameScene.initializePlayerCards(playerCardsPane, cardWidth, cardHeight, distance);
+        playerCardsPane.setLayoutX(15);
+        playerCardsPane.setLayoutY(99);
+        playerMainPane.getChildren().add(playerCardsPane);
+
+        //initialize player text
+        username = new Text(player.getUsername());
+        //username.setFill(convertPlayerColor(player.getColor()));
+        username.setLayoutX(15);
+        username.setLayoutY(26);
+        playerMainPane.getChildren().add(username);
+
+
+        resourcesPane = new Pane();
+        resourcesPane.setPrefSize(resourcesPaneWidth, resourcesPaneHeight);
+        //todo add resources
+        resourcesPane.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, null)));
+        resourcesPane.setLayoutX(15);
+        resourcesPane.setLayoutY(51);
+        playerMainPane.getChildren().add(resourcesPane);
+
+
+        switchPlayground = new Button("PLAYGROUND");
+        switchPlayground.setLayoutX(317);
+        switchPlayground.setLayoutY(99);
+        switchPlayground.setPrefSize(switchPlaygroundWidth, switchPlaygroundHeight);
+        playerMainPane.getChildren().add(switchPlayground);
+
+
+    }
+
+    public void setPlayerCardsPane(Pane playerCardsPane) {
+        this.playerCardsPane = playerCardsPane;
+    }
+
+    private Color convertPlayerColor(PlayerColor color) {
+        switch (color) {
+            case RED -> {
+                return Color.RED;
+            }
+            case BLACK -> {
+                return Color.BLACK;
+            }
+            case BLUE -> {
+                return Color.BLUE;
+            }
+            case GREEN -> {
+                return Color.GREEN;
+            }
+            case YELLOW -> {
+                return Color.YELLOW;
+            }
+        }
+        return null;
+    }
+
+    public Pane getPlayerMainPane() {
+        return playerMainPane;
+    }
+
+
+}
