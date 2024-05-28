de722a9c33e197aca5ef7a998e94abb2a021509f
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-05-28 22:08:34 +0200

    feat(BoardPane): add getter methods

--- src/main/java/it/polimi/ingsw/network/client/view/gui/util/BoardPane.java
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/util/BoardPane.java
@@ -2,7 +2,10 @@ package it.polimi.ingsw.network.client.view.gui.util;
 
 import it.polimi.ingsw.model.Deck.DeckType;
 import it.polimi.ingsw.network.client.model.board.ClientBoard;
+import it.polimi.ingsw.network.client.model.card.ClientFace;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
 
@@ -24,9 +27,39 @@ public class BoardPane {
 
     }
 
-    private void createDeck(DeckType Type){
+    private void createDeck(DeckType Type, ClientFace face){
+        if(Type == DeckType.GOLDEN){
+            goldenDeckTopCard = createBoardRectangle();
+            goldenDeckTopCard.setFill(new ImagePattern(new Image(face.getPath())));
 
+        }
+        else{
+            resourceDeckTopCard = createBoardRectangle();
+            resourceDeckTopCard.setFill(new ImagePattern(new Image(face.getPath())));
+        }
     }
 
+    private Rectangle createBoardRectangle(){
+        return new Rectangle(GUICards.boardCardsWidth, GUICards.boardCardsHeight);
+    }
 
+    public Pane getBoardMainPane() {
+        return BoardMainPane;
+    }
+
+    public Rectangle getGoldenDeckTopCard() {
+        return goldenDeckTopCard;
+    }
+
+    public Rectangle getResourceDeckTopCard() {
+        return resourceDeckTopCard;
+    }
+
+    public List<Rectangle> getResourceFaceUp() {
+        return resourceFaceUp;
+    }
+
+    public List<Rectangle> getGoldenFaceUp() {
+        return goldenFaceUp;
+    }
 }
