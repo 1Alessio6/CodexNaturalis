0eeb6234d5a1c48685239cb12b6901dcc48f5af3
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:23:30 +0200

    feat(PlayerState): add javadoc to PlayerState enumeration

--- src/main/java/it/polimi/ingsw/model/player/action/PlayerState.java
+++ src/main/java/it/polimi/ingsw/model/player/action/PlayerState.java
@@ -1,9 +1,31 @@
 package it.polimi.ingsw.model.player.action;
 
+/**
+ * Enumeration representing the states of the player's actions
+ */
 public enum PlayerState {
+    /**
+     * Choose Starter refers to the state in which the player chooses the starter card side and its placement
+     */
     ChooseStarter,
+
+    /**
+     * Choose Color refers to the state in which the player chooses its color
+     */
     ChooseColor,
+
+    /**
+     * Choose Objective refers to the state in which the player chooses its secret objective card
+     */
     ChooseObjective,
+
+    /**
+     * Place refers to the state in which the player places a card
+     */
     Place,
+
+    /**
+     * Draw refers to the state in which the player draws a card
+     */
     Draw
 }
