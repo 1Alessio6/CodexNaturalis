5e449bbba1a8dd0e012fe168544c9fc6b4e4c0c0
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 22:41:18 +0200

    feat(ChooseColor): add javadoc to ChooseColor class

--- src/main/java/it/polimi/ingsw/model/player/action/ChooseColor.java
+++ src/main/java/it/polimi/ingsw/model/player/action/ChooseColor.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.model.player.action;
 import it.polimi.ingsw.model.card.Color.PlayerColor;
 import it.polimi.ingsw.model.player.Player;
 
+/**
+ * This class is responsible for managing the choice of the token color.
+ */
 public class ChooseColor extends PlayerAction {
     /**
      * {@inheritDoc}
