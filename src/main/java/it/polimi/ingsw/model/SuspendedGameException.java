d653f5bda930a0f4a2932ecef16289b8beffbf13
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:24:45 +0200

    feat(SuspendedGameException): add javadoc to SuspendedGameException

--- src/main/java/it/polimi/ingsw/model/SuspendedGameException.java
+++ src/main/java/it/polimi/ingsw/model/SuspendedGameException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model;
 
+/**
+ * Suspended Game Exception is thrown when the player attempts to perform an action, but the game is suspended
+ */
 public class SuspendedGameException extends Exception {
     public SuspendedGameException() {
         super("You can't play now, the game is suspended");
