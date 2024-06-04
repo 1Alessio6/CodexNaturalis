d8cdbae8a541b728a2c6cc945c997f03256347d1
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:11:10 +0200

    feat(InvalidUsernameException): add javadoc to InvalidUsernameException

--- src/main/java/it/polimi/ingsw/model/lobby/InvalidUsernameException.java
+++ src/main/java/it/polimi/ingsw/model/lobby/InvalidUsernameException.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.model.lobby;
 
+/**
+ * Invalid Username Exception is thrown when the player tries to enter a game using a name that is not present at the
+ * beginning of the game or enters with a name that is already registered in the game
+ */
 public class InvalidUsernameException extends Exception{
     public InvalidUsernameException() {
         super("This username has already been taken, please try with another one");
