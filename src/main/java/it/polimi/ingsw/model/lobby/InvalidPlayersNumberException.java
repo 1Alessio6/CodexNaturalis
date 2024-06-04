098850351969b85efa1171f5c917c6b749b8a493
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:09:56 +0200

    feat(InvalidPlayersNumberException): add javadoc to InvalidPlayersNumberException

--- src/main/java/it/polimi/ingsw/model/lobby/InvalidPlayersNumberException.java
+++ src/main/java/it/polimi/ingsw/model/lobby/InvalidPlayersNumberException.java
@@ -2,6 +2,10 @@ package it.polimi.ingsw.model.lobby;
 
 import it.polimi.ingsw.model.card.InvalidCardIdException;
 
+/**
+ * Invalid Players Number Exception is thrown when the number of players established by the first player is not within
+ * the specified range
+ */
 public class InvalidPlayersNumberException extends Exception {
     public InvalidPlayersNumberException() {
         super("Invalid players number. Set a number between 2 and 4");
