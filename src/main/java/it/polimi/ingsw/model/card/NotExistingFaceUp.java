27029c6500beea0619dfa410c8867f677f45034c
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:15:24 +0200

    feat(NotExistingFaceUp): add javadoc to NotExistingFaceUp exception

--- src/main/java/it/polimi/ingsw/model/card/NotExistingFaceUp.java
+++ src/main/java/it/polimi/ingsw/model/card/NotExistingFaceUp.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.card;
 
+/**
+ * Not Existing Face Up is thrown when in a given position, no face up card is found
+ */
 public class NotExistingFaceUp extends Exception{
     public NotExistingFaceUp()  {
         super("Empty face up position");
