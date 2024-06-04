23bc991c430f2ec545bce18aa0be705fbc428882
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 22:56:04 +0200

    feat(FullLobbyException): add javadoc to FullLobbyException

--- src/main/java/it/polimi/ingsw/model/lobby/FullLobbyException.java
+++ src/main/java/it/polimi/ingsw/model/lobby/FullLobbyException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.lobby;
 
+/**
+ * Full Lobby Exception is thrown when the game hasn't started but the lobby is full
+ */
 public class FullLobbyException extends Exception {
     public FullLobbyException() {
         super("Game hasn't started yet, but lobby is already full");
