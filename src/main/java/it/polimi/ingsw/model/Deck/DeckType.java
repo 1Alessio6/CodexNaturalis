69cf623dc1bc24d70e1cbba062c624ca2f7c433b
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 22:54:45 +0200

    feat(DeckType): add javadoc to DeckType enumeration

--- src/main/java/it/polimi/ingsw/model/Deck/DeckType.java
+++ src/main/java/it/polimi/ingsw/model/Deck/DeckType.java
@@ -1,6 +1,17 @@
 package it.polimi.ingsw.model.Deck;
 
+/**
+ * Enumeration representing the type of the deck
+ */
 public enum DeckType {
+
+    /**
+     * GOLDEN, if the deck is made up of golden cards
+     */
     GOLDEN,
+
+    /**
+     * RESOURCE, if the deck is made up of resource cards
+     */
     RESOURCE
 }
