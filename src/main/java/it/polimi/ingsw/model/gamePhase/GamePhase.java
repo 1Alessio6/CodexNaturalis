1b8bd6ba4bb4c3da3106634a3f6f90f3ecd9f49d
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 22:57:18 +0200

    feat(GamePhase): add javadoc to GamePhase enumeration

--- src/main/java/it/polimi/ingsw/model/gamePhase/GamePhase.java
+++ src/main/java/it/polimi/ingsw/model/gamePhase/GamePhase.java
@@ -8,9 +8,29 @@ import java.io.Serializable;
  */
 
 public enum GamePhase implements Serializable {
+
+    /**
+     * Set up refers to the phase in which the username, color, starter and objective card have been selected
+     */
     Setup,
+
+    /**
+     * Place Normal refers to the phase in which the player is allowed to place a card in a normal situation
+     */
     PlaceNormal,
+
+    /**
+     * Draw Normal refers to the phase in which the player is allowed to draw a card in a normal situation
+     */
     DrawNormal,
+
+    /**
+     * Place Additional refers to the phase during which the player is allowed to place a card once 20 points have been achieved
+     */
     PlaceAdditional,
+
+    /**
+     * End refers to the ending phase
+     */
     End
 }
