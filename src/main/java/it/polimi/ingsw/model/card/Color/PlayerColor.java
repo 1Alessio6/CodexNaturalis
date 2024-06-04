f8f73980dd5678219106b8d2bb00a8cf3d459520
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:22:29 +0200

    feat(PlayerColor): add javadoc to PlayerColor enumeration

--- src/main/java/it/polimi/ingsw/model/card/Color/PlayerColor.java
+++ src/main/java/it/polimi/ingsw/model/card/Color/PlayerColor.java
@@ -1,9 +1,32 @@
 package it.polimi.ingsw.model.card.Color;
 
+/**
+ * Enumeration representing the possible token colors of the players
+ */
 public enum PlayerColor {
+
+    /**
+     * RED, stands for the red token color
+     */
     RED,
+
+    /**
+     * BLUE, stands for the blue token color
+     */
     BLUE,
+
+    /**
+     * GREEN, stands for the green token color
+     */
     GREEN,
+
+    /**
+     * YELLOW, stands for the yellow token color
+     */
     YELLOW,
+
+    /**
+     * BLACK, stands for the black token color
+     */
     BLACK
 }
