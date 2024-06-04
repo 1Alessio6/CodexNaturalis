3d7ce4cc2481908240045e59ed698b06b36ea5e3
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 22:38:05 +0200

    feat(CardColor): add javadoc to CardColor enumeration

--- src/main/java/it/polimi/ingsw/model/card/Color/CardColor.java
+++ src/main/java/it/polimi/ingsw/model/card/Color/CardColor.java
@@ -1,9 +1,32 @@
 package it.polimi.ingsw.model.card.Color;
 
+/**
+ * Enumeration representing the possible colors of the cards.
+ */
 public enum CardColor {
+
+    /**
+     * RED, if the card is red
+     */
     RED,
+
+    /**
+     * GREEN, if the card is green
+     */
     GREEN,
+
+    /**
+     * BLUE, if the card is blue
+     */
     BLUE,
+
+    /**
+     * YELLOW, if the card is yellow
+     */
     YELLOW,
+
+    /**
+     * PURPLE, if the card is purple
+     */
     PURPLE
 }
