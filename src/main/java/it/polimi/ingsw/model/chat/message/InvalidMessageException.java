ba57c86678769f897580f8681a6028a5341432ee
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-04 23:04:33 +0200

    feat(InvalidMessageException): add javadoc to InvalidMessageException

--- src/main/java/it/polimi/ingsw/model/chat/message/InvalidMessageException.java
+++ src/main/java/it/polimi/ingsw/model/chat/message/InvalidMessageException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.chat.message;
 
+/**
+ * Invalid Message Exception is thrown when the author doesn't match the sender or the recipient is an invalid username
+ */
 public class InvalidMessageException extends Exception {
     public InvalidMessageException() {
         super("Invalid message");
