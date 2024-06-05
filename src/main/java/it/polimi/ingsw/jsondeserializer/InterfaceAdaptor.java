5b51302b19a42b889c7baee833f0cfd61a26e8b6
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-06 00:21:13 +0200

    feat(InterfaceAdaptor): add javadoc to InterfaceAdaptor class

--- src/main/java/it/polimi/ingsw/jsondeserializer/InterfaceAdaptor.java
+++ src/main/java/it/polimi/ingsw/jsondeserializer/InterfaceAdaptor.java
@@ -4,12 +4,18 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
+/**
+ * Custom interface adapter used for serializing and deserializing
+ */
 public class InterfaceAdaptor<T> implements JsonSerializer<T>, JsonDeserializer<T> {
 
     private static final String CLASS_IDENTIFIER = "concreteType";
 
     private static final String ATTRIBUTES = "attributes";
 
+    /**
+     * {@inheritDoc}
+     */
     public T deserialize(JsonElement jsonElement, Type type,
                          JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
 
@@ -21,6 +27,9 @@ public class InterfaceAdaptor<T> implements JsonSerializer<T>, JsonDeserializer<
         return jsonDeserializationContext.deserialize(jsonObject.get(ATTRIBUTES), objectClass);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public JsonElement serialize(T objToSerialize, Type type, JsonSerializationContext jsonSerializationContext) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty(CLASS_IDENTIFIER, objToSerialize.getClass().getName()); // get the class name not including the package
