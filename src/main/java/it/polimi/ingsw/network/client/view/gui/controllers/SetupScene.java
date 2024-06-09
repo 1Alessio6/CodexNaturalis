3c69c2012e1ab4cded3c115c026d3e3ec7145836
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-06-09 16:09:05 +0200

    feat(SetupScene): add methods to handle notifications from the server

--- src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/SetupScene.java
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/controllers/SetupScene.java
@@ -1,14 +1,23 @@
 package it.polimi.ingsw.network.client.view.gui.controllers;
 
-import it.polimi.ingsw.network.client.Client;
+import it.polimi.ingsw.model.InvalidGamePhaseException;
+import it.polimi.ingsw.model.SuspendedGameException;
+import it.polimi.ingsw.model.card.Color.InvalidColorException;
+import it.polimi.ingsw.model.card.Color.PlayerColor;
 import it.polimi.ingsw.network.client.controller.ClientController;
 import it.polimi.ingsw.network.client.model.card.ClientCard;
 import it.polimi.ingsw.network.client.model.player.ClientPlayer;
 import it.polimi.ingsw.network.client.view.gui.circle.GUICircle;
 import it.polimi.ingsw.network.client.view.gui.util.GUICards;
+import it.polimi.ingsw.network.client.view.gui.util.GUIUtil;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
+import java.awt.*;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,14 +27,42 @@ public class SetupScene extends SceneController {
 
     @FXML
     private Rectangle secondRectangle;
+    @FXML
+    private TextField text;
 
     private List<GUICircle> colors;
-
-    private int numColorsLeft;
 
     static private final double xRefForCircleCenter = 418;
     static private final double yRefForCircleCenter = 554;
     static private final double radius = 35;
+
+    private void initializeColors() {
+        ClientController clientController = gui.getController();
+        colors = new ArrayList<>();
+
+
+        List<PlayerColor> availableColors = clientController.getAvailableColors();
+        double x = xRefForCircleCenter + 2 * radius * (4 - availableColors.size());
+        for (PlayerColor availableColor : availableColors) {
+            GUICircle circle = new GUICircle(x, yRefForCircleCenter, radius, availableColor);
+            circle.getCircle().setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    if (GUIUtil.isClicked(mouseEvent, MouseButton.PRIMARY)) {
+                        try {
+                            gui.getController().chooseColor(availableColor);
+                            // todo. change with specific report error
+                        } catch (InvalidColorException | RemoteException | InvalidGamePhaseException |
+                                 SuspendedGameException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            });
+            colors.add(circle);
+            x += 2 * radius;
+        }
+    }
 
     @Override
     public void initialize() {
@@ -36,21 +73,33 @@ public class SetupScene extends SceneController {
         secondRectangle.setFill(GUICards.pathToImage(starter.getBackPath()));
         firstRectangle.setVisible(true);
         secondRectangle.setVisible(true);
-
-        numColorsLeft = clientController.getAvailableColors().size();
-
-        colors = new ArrayList<>();
-        double x = xRefForCircleCenter + 2 * radius * numColorsLeft;
-        for (int i = 0; i < numColorsLeft; ++i) {
-            colors.add(new GUICircle(x, yRefForCircleCenter, radius));
-            x += 2 * radius;
-        }
+        text.setText("Choose your starter");
+        initializeColors();
     }
 
     public SetupScene() {
-
     }
 
+    private void showColors() {
+        for (GUICircle circle : colors) {
+            circle.setVisibility(true);
+        }
+    }
 
+    private void updateColorDisposition() {
+        colors.getLast().setVisibility(false);
+        colors.removeLast();
+        for (GUICircle c : colors) {
+            c.setCoordinates(c.getCircle().getCenterX() + 2*radius, c.getCircle().getCenterY());
+        }
+    }
+
+    private void updateAfterColorChoiceHasBeenAccepted() {
+        for (GUICircle circle : colors) {
+            circle.setVisibility(false);
+        }
+        firstRectangle.setVisible(true);
+        secondRectangle.setVisible(true);
+    }
 
 }
