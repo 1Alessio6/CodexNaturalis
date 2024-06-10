009577f71fb4be4ab743090d6a5a0fd6b6fb5eee
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-06-10 15:25:50 +0200

    feat(Symbol): add path to png images

--- src/main/java/it/polimi/ingsw/model/card/Symbol.java
+++ src/main/java/it/polimi/ingsw/model/card/Symbol.java
@@ -8,45 +8,51 @@ public enum Symbol {
     /**
      * Plant, refers to the resource that belong to the Plant kingdom.
      */
-    PLANT("\uD83C\uDF43"),
+    PLANT("\uD83C\uDF43", "/gui/png/symbols/PLANT.png"),
 
     /**
      * Fungi, refers to the resource that belong to the Fungi kingdom.
      */
-    FUNGI("\uD83C\uDF44"),
+    FUNGI("\uD83C\uDF44", "/gui/png/symbols/FUNGI.png"),
 
     /**
      * Animal, refers to the resource that belong to the Animal kingdom.
      */
-    ANIMAL("\uD83D\uDC3A"),
+    ANIMAL("\uD83D\uDC3A", "/gui/png/symbols/ANIMAL.png"),
 
     /**
      * Insect, refers to the resource that belong to the Insect kingdom.
      */
-    INSECT("\uD83E\uDD8B"),
+    INSECT("\uD83E\uDD8B","/gui/png/symbols/INSECT.png"),
 
     /**
      * Quill, refers to the Quill object.
      */
-    QUILL("\uD83E\uDEB6"),
+    QUILL("\uD83E\uDEB6","/gui/png/symbols/QUILL.png"),
 
     /**
      * Inkwell, refers to the Inkwell object.
      */
-    INKWELL("\uD83E\uDED9"),
+    INKWELL("\uD83E\uDED9","/gui/png/symbols/INKWELL.png"),
 
     /**
      * Manuscript, refers to the Manuscript object.
      */
-    MANUSCRIPT("\uD83D\uDCDC");
+    MANUSCRIPT("\uD83D\uDCDC", "/gui/png/symbols/MANUSCRIPT.png");
 
     final String icon;
+    final String pngPath;
 
-    Symbol(String icon) {
+    Symbol(String icon, String pngPath) {
         this.icon = icon;
+        this.pngPath = pngPath;
     }
 
     public String getIcon() {
         return this.icon;
+    }
+
+    public String getPath() {
+        return pngPath;
     }
 }
