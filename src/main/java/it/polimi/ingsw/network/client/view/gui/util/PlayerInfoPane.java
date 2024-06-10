079b99798b5314367c50916ae2f9ef4eaf82fcc0
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-06-10 15:26:10 +0200

    feat(PlayerInfoPane): add initializeResources method

--- src/main/java/it/polimi/ingsw/network/client/view/gui/util/PlayerInfoPane.java
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/util/PlayerInfoPane.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.network.client.view.gui.util;
 
 import it.polimi.ingsw.model.card.Color.PlayerColor;
+import it.polimi.ingsw.model.card.Symbol;
 import it.polimi.ingsw.network.client.model.card.ClientCard;
 import it.polimi.ingsw.network.client.model.player.ClientPlayer;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -13,8 +15,11 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static it.polimi.ingsw.network.client.view.gui.util.GUIUtil.convertPlayerColor;
 import static it.polimi.ingsw.network.client.view.gui.util.GUIUtil.setBackgroundColor;
 
 public class PlayerInfoPane {
@@ -51,8 +56,11 @@ public class PlayerInfoPane {
 
     private static final int cardHeight = 48;
 
+    private final Map<Symbol, Text> resources;
+
 
     public PlayerInfoPane(ClientPlayer player) {
+        resources = new HashMap<>();
         playerMainPane = new Pane();
         playerMainPane.setBackground(setBackgroundColor("#EEE5BC"));
         playerMainPane.setPrefSize(mainPaneWidth, mainPaneHeight);
@@ -75,7 +83,7 @@ public class PlayerInfoPane {
 
         //initialize player text
         username = new Text(player.getUsername());
-        //username.setFill(convertPlayerColor(player.getColor()));
+        username.setFill(convertPlayerColor(player.getColor()));
         username.setLayoutX(15);
         username.setLayoutY(26);
         playerMainPane.getChildren().add(username);
@@ -84,7 +92,8 @@ public class PlayerInfoPane {
         resourcesPane = new Pane();
         resourcesPane.setPrefSize(resourcesPaneWidth, resourcesPaneHeight);
         //todo add resources
-        resourcesPane.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, null)));
+        resourcesPane.setBackground(setBackgroundColor("#FFFFFF"));
+        initializeResources();
         resourcesPane.setLayoutX(15);
         resourcesPane.setLayoutY(51);
         playerMainPane.getChildren().add(resourcesPane);
@@ -103,7 +112,24 @@ public class PlayerInfoPane {
         this.playerCardsPane = playerCardsPane;
     }
 
+    private void initializeResources() {
 
+        double layoutX = 10.0;
+
+        for (Symbol symbol : Symbol.values()) {
+            ImageView symbolImage = new ImageView(symbol.getPath());
+            symbolImage.setFitHeight(33.6);
+            symbolImage.setFitWidth(30.45);
+            symbolImage.setLayoutX(layoutX);
+            Text points = new Text("0");
+            resources.put(symbol, points);
+            points.setLayoutX(layoutX + 30.45 + 5);
+            points.setLayoutY(22);
+            resourcesPane.getChildren().add(symbolImage);
+            resourcesPane.getChildren().add(points);
+            layoutX = layoutX + 50.0;
+        }
+    }
 
     public void updatePlayerCards(List<ClientCard> cards) {
         GUICards.initializePlayerCards(playerCardsPane, cards, cardWidth, cardHeight, distance, MouseButton.PRIMARY);
@@ -113,7 +139,7 @@ public class PlayerInfoPane {
         return playerMainPane;
     }
 
-    public String getPlayerUsername(){
+    public String getPlayerUsername() {
         return username.getText();
     }
 }
