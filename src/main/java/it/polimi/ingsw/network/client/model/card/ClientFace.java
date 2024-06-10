7678a19cc93620121a8bc1214767c601c6e1acb7
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-10 22:56:09 +0200

    feat(ClientFace): add javadoc to ClientFace class

--- src/main/java/it/polimi/ingsw/network/client/model/card/ClientFace.java
+++ src/main/java/it/polimi/ingsw/network/client/model/card/ClientFace.java
@@ -7,6 +7,10 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents the Face card in the client's side.
+ * The face can be either the front or the back.
+ */
 public class ClientFace implements Serializable {
     private final int faceID;
 
@@ -26,6 +30,11 @@ public class ClientFace implements Serializable {
         faceID = -1;
     }
 
+    /**
+     * Constructs a face card with the <code>face</code> provided.
+     *
+     * @param face from which the face card is to be constructed.
+     */
     public ClientFace(Face face) {
         faceID = face.getId();
         corners = face.getCorners();
@@ -36,6 +45,10 @@ public class ClientFace implements Serializable {
         requirements = face.getRequiredResources();
     }
 
+    /**
+     * Constructs a face given a <code>faceID</code>.
+     * @param faceID from which the face card is to be constructed.
+     */
     public ClientFace(int faceID) {
         this.faceID = faceID;
         this.corners = new HashMap<>();
