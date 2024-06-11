f311a9193e0e64a58172f6c6ffc5c2f9a8ef1a26
Alessio Ginolfi <alessio.ginolfi@mail.polimi.it>
2024-06-11 15:49:03 +0200

    feat(gui): add Icon enum

---
+++ src/main/java/it/polimi/ingsw/network/client/view/gui/util/Icon.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.network.client.view.gui.util;
+
+public enum Icon {
+
+    OBSERVE_PLAYGROUND("/gui/png/icons/observe_playground.png"),
+
+    SETTINGS("/gui/icons/.png");
+
+
+
+    private final String path;
+
+    Icon(String path) {
+        this.path = path;
+    }
+
+    public String getPath(){
+        return path;
+    }
+
+}
