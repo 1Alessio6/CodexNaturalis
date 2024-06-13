4fdd611716d9a6bdeffe10d45ec80384b49225f8
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-06-13 15:50:58 +0200

    feat: add constructor which takes message from specific exception.

--- src/main/java/it/polimi/ingsw/network/client/UnReachableServerException.java
+++ src/main/java/it/polimi/ingsw/network/client/UnReachableServerException.java
@@ -4,4 +4,8 @@ public class UnReachableServerException extends Exception {
     public UnReachableServerException() {
         super("Unreachable server");
     }
+
+    public UnReachableServerException(String message) {
+        super(message);
+    }
 }
