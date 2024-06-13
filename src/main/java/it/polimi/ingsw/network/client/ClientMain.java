25cc0a84c85e5058d014bff66e58a5227af39b16
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-06-13 15:39:55 +0200

    refactor: let the view take the server ip and available port.

--- src/main/java/it/polimi/ingsw/network/client/ClientMain.java
+++ src/main/java/it/polimi/ingsw/network/client/ClientMain.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.network.client;
 
+import it.polimi.ingsw.network.client.controller.ClientController;
 import it.polimi.ingsw.network.client.rmi.ClientRMI;
 import it.polimi.ingsw.network.client.socket.ClientSocket;
+import it.polimi.ingsw.network.client.view.View;
 import it.polimi.ingsw.network.client.view.gui.ApplicationGUI;
 import it.polimi.ingsw.network.client.view.tui.ClientUtil;
 
@@ -15,28 +17,23 @@ public class ClientMain {
     private static final String OPTION_TUI = "tui";
     private static final String OPTION_GUI = "gui";
 
-    public static Client createClient(List<String> params) throws UnReachableServerException, RemoteException {
+    public static Client createClient(String typeConnection) {
         Client client;
-        String typeConnection = params.get(0);
-        String host = params.get(1);
-        Integer port = Integer.parseInt(params.get(2));
         if (typeConnection.equals(OPTION_RMI)) {
-            client = new ClientRMI(host, port);
+            client = new ClientRMI();
         } else {
-            client = new ClientSocket(host, port);
+            client = new ClientSocket();
         }
         return client;
     }
 
     public static void main(String[] args) {
-        String typeConnection = OPTION_SOCKET;
+        String typeConnection = OPTION_RMI;
         String typeView = OPTION_GUI;
-        String host = "localhost";
-        String port = "1234";
-
         try {
-            // max 4 args: rmi tui port <int>
-            if (args.length > 0 && args.length <= 6) {
+            int maxArgs = 2; // <connectionType> + <viewType>
+            // max 4 args: rmi tui
+            if (args.length > 0 && args.length <= maxArgs) {
                 // override stuff
                 if (Arrays.stream(args).anyMatch("--rmi"::equalsIgnoreCase)) {
                     typeConnection = OPTION_RMI;
@@ -44,16 +41,16 @@ public class ClientMain {
                 if (Arrays.stream(args).anyMatch("--tui"::equalsIgnoreCase)) {
                     typeView = OPTION_TUI;
                 }
-                for (int i = 0; i < args.length; i++) {
-                    if (args[i].equalsIgnoreCase("--port")) {
-                        port = args[i + 1];
-                    }
+                //for (int i = 0; i < args.length; i++) {
+                //    if (args[i].equalsIgnoreCase("--port")) {
+                //        port = args[i + 1];
+                //    }
 
-                    if (args[i].equalsIgnoreCase("--host")) {
-                        host = args[i + 1];
-                    }
-                }
-            } else if (args.length > 6) {
+                //    if (args[i].equalsIgnoreCase("--host")) {
+                //        host = args[i + 1];
+                //    }
+                //}
+            } else if (args.length > maxArgs) {
                 ClientUtil.argsHelper("Too many arguments");
             }
             ClientApplication application;
@@ -62,7 +59,8 @@ public class ClientMain {
             } else {
                 application = new ApplicationGUI();
             }
-            application.run(typeConnection, host, port);
+            //application.run(typeConnection, host, port);
+            application.run(typeConnection);
         } catch (UnReachableServerException | RemoteException e) {
             System.err.println(e.getMessage());
             System.exit(1);
