f1f274722f57ffec255274ae3e3f2c9c81f5d5e7
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-13 22:53:02 +0200

    feat(ClientRMI): add javadoc to ClientRMI class

--- src/main/java/it/polimi/ingsw/network/client/rmi/ClientRMI.java
+++ src/main/java/it/polimi/ingsw/network/client/rmi/ClientRMI.java
@@ -26,10 +26,20 @@ import java.util.Map;
 public class ClientRMI extends Client {
     private HeartBeat heartBeat;
 
+    /**
+     * Constructs the ClientRMI using the <code>host</code> and the <code>port</code> provided.
+     *
+     * @param host the name of the host to connect to.
+     * @param port the port number.
+     * @throws UnReachableServerException if the server isn't reachable.
+     */
     public ClientRMI(String host, Integer port) throws UnReachableServerException {
         super(host, port);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void connect(String ip, Integer port) throws UnReachableServerException {
         String serverName = ServerRMI.getServerName();
@@ -41,6 +51,9 @@ public class ClientRMI extends Client {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void runView() throws RemoteException {
         // register stub
@@ -50,6 +63,9 @@ public class ClientRMI extends Client {
         clientView.runView(stub);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setName(String name) {
         this.name = name;
@@ -57,40 +73,61 @@ public class ClientRMI extends Client {
         heartBeat.startHeartBeat();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void updateCreator() throws RemoteException {
         clientView.showUpdateCreator();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void updateAfterLobbyCrash() throws RemoteException {
         clientView.showUpdateAfterLobbyCrash();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void updateAfterConnection(ClientGame clientGame) {
         controller.updateAfterConnection(clientGame);
         clientView.showUpdateAfterConnection();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdatePlayersInLobby(List<String> usernames) throws RemoteException {
         controller.updatePlayersInLobby(usernames);
         clientView.showUpdatePlayersInLobby();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdatePlayerStatus(boolean isConnected, String username) throws RemoteException {
         controller.updatePlayerStatus(isConnected, username);
         clientView.showUpdatePlayerStatus();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateColor(PlayerColor color, String username) throws RemoteException {
         controller.updateColor(color, username);
         clientView.showUpdateColor(username);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateObjectiveCard(ClientObjectiveCard chosenObjective, String username) {
         controller.updateObjectiveCard(chosenObjective, username);
@@ -100,6 +137,9 @@ public class ClientRMI extends Client {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateAfterPlace(Map<Position, CornerPosition> positionToCornerCovered, List<Position> newAvailablePositions, Map<Symbol, Integer> newResources, int points, String username, ClientCard placedCard, Side placedSide, Position position) throws RemoteException {
         controller.updateAfterPlace(positionToCornerCovered, newAvailablePositions, newResources, points, username, placedCard, placedSide, position);
@@ -111,12 +151,18 @@ public class ClientRMI extends Client {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateAfterDraw(ClientCard drawnCard, ClientFace newTopDeck, ClientCard newFaceUpCard, String username, int boardPosition) throws RemoteException {
         controller.updateAfterDraw(drawnCard,newTopDeck,newFaceUpCard,username,boardPosition);
         clientView.showUpdateAfterDraw();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateChat(Message message) throws RemoteException {
         controller.updateChat(message);
@@ -128,34 +174,52 @@ public class ClientRMI extends Client {
 
     //}
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateCurrentPlayer(int currentPlayerIdx, GamePhase phase) throws RemoteException {
         controller.updateCurrentPlayer(currentPlayerIdx,phase);
         clientView.showUpdateCurrentPlayer();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showUpdateGameState() throws RemoteException {
         controller.updateSuspendedGame();
         clientView.showUpdateSuspendedGame();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void showWinners(List<String> winners) throws RemoteException {
         clientView.showWinners(winners);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void reportError(String details) throws RemoteException {
         System.err.println(details);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void handleUnresponsiveness(String unresponsiveListener) {
         clientView.showServerCrash();
         System.exit(1);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void receivePing(HeartBeatMessage ping) throws RemoteException {
         heartBeat.registerMessage(ping);
