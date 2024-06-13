0c635109188990cf0331db35e049c31554364995
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-13 21:56:03 +0200

    feat(HeartBeatHandler): add javadoc to handleUnresponsiveness class

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatHandler.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatHandler.java
@@ -5,5 +5,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface HeartBeatHandler extends Remote {
+
+    /**
+     * Handles the unresponsiveness of clients.
+     *
+     * @param unresponsiveListener the unresponsiveness client.
+     * @throws RemoteException in the event of an error occurring during the execution of a remote method.
+     */
     void handleUnresponsiveness(String unresponsiveListener) throws RemoteException;
 }
