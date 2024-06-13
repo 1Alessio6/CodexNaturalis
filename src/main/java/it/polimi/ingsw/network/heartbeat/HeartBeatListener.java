e9ad414a31213cc450e02effb65054fad2bf3e62
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-13 21:56:34 +0200

    feat(HeartBeatListener): add javadoc to receivePing class

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatListener.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatListener.java
@@ -4,5 +4,11 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface HeartBeatListener extends Remote{
+    /**
+     * Receives pings from clients
+     *
+     * @param ping the ping from the client
+     * @throws RemoteException in the event of an error occurring during the execution of a remote method.
+     */
     void receivePing(HeartBeatMessage ping) throws RemoteException;
 }
