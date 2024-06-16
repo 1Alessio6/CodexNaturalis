d91faaf9b241191bdb1abe8288a6c8fade105fe3
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 23:19:59 +0200

    feat(CalculateNoCondition): add javadoc to calculatePoints method

--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculateNoCondition.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculateNoCondition.java
@@ -7,6 +7,13 @@ import it.polimi.ingsw.model.board.Position;
  * Strategy used to calculate points in a card that doesn't require to fulfill any condition
  */
 public class CalculateNoCondition implements CalculatePoints{
+    /**
+     * Calculates the points in a card when it doesn't have points condition
+     *
+     * @param pos        the position in the playground.
+     * @param playground the playground on which the calculation is to be carried.
+     * @return the calculated points.
+     */
     @Override
     public int calculatePoints(Position pos, Playground playground) {
         return playground.getTile(pos).getFace().getScore();
