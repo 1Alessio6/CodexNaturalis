00a9739a79cf2522bd532662f4d54de2ee52e1c1
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:59:58 +0200

    feat(UpdateSuspendedGameMessage): add javadoc to UpdateSuspendedGameMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateSuspendedGameMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateSuspendedGameMessage.java
@@ -3,7 +3,13 @@ package it.polimi.ingsw.network.client.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * UpdateSuspendedGameMessage represents the message indicating the suspension of the game.
+ */
 public class UpdateSuspendedGameMessage extends NetworkMessage {
+    /**
+     * Constructs an <code>UpdateSuspendedGameMessage</code>.
+     */
     public UpdateSuspendedGameMessage() {
         super(Type.SHOW_UPDATE_SUSPENDED_GAME, "server");
     }
