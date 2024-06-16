0740e6c8e4d045049dcae0b3b76dad45e2ab4f04
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:52:52 +0200

    feat(UpdateAfterLobbyCrashMessage): add javadoc to UpdateAfterLobbyCrashMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateAfterLobbyCrashMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateAfterLobbyCrashMessage.java
@@ -3,8 +3,14 @@ package it.polimi.ingsw.network.client.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * UpdateAfterLobbyCrashMessage represents the message indicating the crash of the lobby.
+ */
 public class UpdateAfterLobbyCrashMessage extends NetworkMessage {
 
+    /**
+     * Constructs an <code>UpdateAfterLobbyCrashMessage</code>.
+     */
     public UpdateAfterLobbyCrashMessage() {
         super(Type.UPDATE_AFTER_LOBBY_CRASH, "server");
     }
