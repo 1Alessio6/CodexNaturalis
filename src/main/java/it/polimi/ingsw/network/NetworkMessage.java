d87adc26c6abac3e3caf3d991eb97ae18a9c91c7
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:33:17 +0200

    feat(NetworkMessage): add javadoc to NetworkMessage class

--- src/main/java/it/polimi/ingsw/network/NetworkMessage.java
+++ src/main/java/it/polimi/ingsw/network/NetworkMessage.java
@@ -2,11 +2,20 @@ package it.polimi.ingsw.network;
 
 import java.io.Serializable;
 
+/**
+ * Network Message represents the messages passed through the network.
+ */
 public class NetworkMessage implements Serializable {
     Type networkType;
 
     String sender;
 
+    /**
+     * Constructs a <code>NetworkMessage</code>.
+     *
+     * @param type   the network type.
+     * @param sender the sender of the network message.
+     */
     public NetworkMessage(Type type, String sender) {
         this.networkType = type;
         this.sender = sender;
