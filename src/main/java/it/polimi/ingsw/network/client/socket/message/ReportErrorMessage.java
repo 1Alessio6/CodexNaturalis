08c21d5573824bfbd3313085b0f04df59a08936b
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:37:20 +0200

    feat(ReportErrorMessage): add javadoc to ReportErrorMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/ReportErrorMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/ReportErrorMessage.java
@@ -3,10 +3,18 @@ package it.polimi.ingsw.network.client.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * ReportErrorMessage represents the message containing the error details.
+ */
 public class ReportErrorMessage extends NetworkMessage {
 
     private final String details;
 
+    /**
+     * Constructs a <code>ReportErrorMessage</code> with the <code>details</code> provided.
+     *
+     * @param details the details of the error.
+     */
     public ReportErrorMessage(String details) {
         super(Type.ERROR, "server");
         this.details = details;
