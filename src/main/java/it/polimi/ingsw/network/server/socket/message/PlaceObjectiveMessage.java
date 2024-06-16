1d823a6db3b4a75df6a018d53ec5a31bb3c1c9ad
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:35:22 +0200

    feat(PlaceObjectiveMessage): add javadoc to PlaceObjectiveMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/PlaceObjectiveMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/PlaceObjectiveMessage.java
@@ -3,9 +3,19 @@ package it.polimi.ingsw.network.server.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * PlaceObjectiveMessage represents the message containing the objective card chosen by the player.
+ */
 public class PlaceObjectiveMessage extends NetworkMessage {
     private int chosenObjective;
 
+    /**
+     * Constructs <code>PlaceObjectiveMessage</code> with the <code>sender</code> and <code>chosenObjective</code>
+     * provided.
+     *
+     * @param sender          the username of the player who chose the <code>chosenObjective</code>.
+     * @param chosenObjective the objective card chosen by the <code>sender</code>.
+     */
     public PlaceObjectiveMessage(String sender, int chosenObjective) {
         super(Type.PLACE_OBJECTIVE, sender);
         this.chosenObjective = chosenObjective;
