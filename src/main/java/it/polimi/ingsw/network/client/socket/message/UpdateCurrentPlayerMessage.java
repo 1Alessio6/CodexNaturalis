79503d6ec36f52f7f5f5f9884c995730aef9a497
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:56:32 +0200

    feat(UpdateCurrentPlayerMessage): add javadoc to UpdateCurrentPlayerMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateCurrentPlayerMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateCurrentPlayerMessage.java
@@ -4,10 +4,21 @@ import it.polimi.ingsw.model.gamePhase.GamePhase;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * UpdateCurrentPlayerMessage represents the message containing the new current player and his current phase.
+ */
 public class UpdateCurrentPlayerMessage extends NetworkMessage {
 
     private final int currentPlayerIdx;
     private final GamePhase currentPhase;
+
+    /**
+     * Constructs an <code>UpdateCurrentPlayerMessage</code> with the <code>currentPlayerIdx</code> and the
+     * <code>currentPhase</code> provided.
+     *
+     * @param currentPlayerIdx the index of the current player.
+     * @param currentPhase     the phase in which the current player is.
+     */
     public UpdateCurrentPlayerMessage(int currentPlayerIdx, GamePhase currentPhase) {
         super(Type.SHOW_UPDATE_CURRENT_PLAYER, "server");
         this.currentPlayerIdx = currentPlayerIdx;
