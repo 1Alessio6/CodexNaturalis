03d7f62a155e03d1461920956cd743aa596bf8ec
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:58:16 +0200

    feat(UpdatePlayersInLobbyMessage): add javadoc to UpdatePlayersInLobbyMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdatePlayersInLobbyMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdatePlayersInLobbyMessage.java
@@ -5,10 +5,18 @@ import it.polimi.ingsw.network.Type;
 
 import java.util.List;
 
+/**
+ * UpdatePlayersInLobbyMessage represents the message containing the new players to add to the lobby.
+ */
 public class UpdatePlayersInLobbyMessage extends NetworkMessage {
 
     private final List<String> usernames;
 
+    /**
+     * Constructs an <code>UpdatePlayersInLobbyMessage</code> with the <code>usernames</code> provided.
+     *
+     * @param usernames the usernames of the players to add to the lobby.
+     */
     public UpdatePlayersInLobbyMessage(List<String> usernames) {
         super(Type.SHOW_UPDATE_PLAYERS_IN_LOBBY, "server");
         this.usernames = usernames;
