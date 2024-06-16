6e1597496eceb62c3110f4c09c469b2047e107f0
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:32:14 +0200

    feat(HeartBeatMessage): add javadoc to HeartBeatMessage class

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatMessage.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeatMessage.java
@@ -6,6 +6,9 @@ import it.polimi.ingsw.network.Type;
 import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * HeartBeatMessage represents the message containing the sender and the id number of the message.
+ */
 public class HeartBeatMessage extends NetworkMessage implements Serializable{
     private final int id;
     //private static final AtomicInteger incrementalId = new AtomicInteger();
@@ -24,6 +27,12 @@ public class HeartBeatMessage extends NetworkMessage implements Serializable{
   //      //this.id = getIncrementedId();
   //  }
 
+    /**
+     * Constructs a <code>HeartBeatMessage</code> with the <code>sender</code> and the <code>id</code> provided.
+     *
+     * @param sender the sender of the heartBeat.
+     * @param id     the identification of the heartBeat message.
+     */
     public HeartBeatMessage(String sender, int id) {
         super(Type.HEARTBEAT, sender);
         this.id = id;
