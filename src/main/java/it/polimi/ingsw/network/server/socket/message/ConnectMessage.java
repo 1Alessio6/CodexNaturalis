6e092a84890edd99cf9b2fd9c4c97d19abd9a12a
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:29:06 +0200

    feat(ConnectMessage): add javadoc to ConnectMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/ConnectMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/ConnectMessage.java
@@ -3,7 +3,16 @@ package it.polimi.ingsw.network.server.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * ConnectMessage represents the message that allows a player to connect to the game.
+ */
 public class ConnectMessage extends NetworkMessage {
+
+    /**
+     * Constructs a <code>ConnectMessage</code> with the <code>sender</code> provided.
+     *
+     * @param sender the username of the player to connect.
+     */
     public ConnectMessage(String sender) {
         super(Type.CONNECT, sender);
     }
