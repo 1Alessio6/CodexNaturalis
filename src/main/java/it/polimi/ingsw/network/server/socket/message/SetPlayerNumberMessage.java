484d75f600fb4d44cc4f55a3efc4b53d64ea1cee
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:45:36 +0200

    feat(SetPlayerNumberMessage): add javadoc to SetPlayerNumberMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/SetPlayerNumberMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/SetPlayerNumberMessage.java
@@ -3,9 +3,18 @@ package it.polimi.ingsw.network.server.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * SetPlayerNumberMessage represents the message containing the number of players.
+ */
 public class SetPlayerNumberMessage extends NetworkMessage {
     private int numPlayers;
 
+    /**
+     * Constructs a <code>SetPlayerNumberMessage</code> with the <code>sender</code> and <code>numPlayers</code> provided.
+     *
+     * @param sender     the username of the player who set the number of players, that is, the name of the first player.
+     * @param numPlayers the number of players chosen by the first player.
+     */
     public SetPlayerNumberMessage(String sender, int numPlayers) {
         super(Type.SET_PLAYER_NUMBER, sender);
         this.numPlayers = numPlayers;
