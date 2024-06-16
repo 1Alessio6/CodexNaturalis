4fadff7b1f8973a64b2c2f8e49962c2569552f10
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:38:08 +0200

    feat(SendChatMessage): add javadoc to SendChatMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/SendChatMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/SendChatMessage.java
@@ -4,9 +4,18 @@ import it.polimi.ingsw.model.chat.message.Message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * SendChatMessage represents the message containing the message to be <B>sent</B> in chat.
+ */
 public class SendChatMessage extends NetworkMessage {
     Message message;
 
+    /**
+     * Constructs a <code>SendChatMessage</code> with the <code>sender</code> and <code>message</code> provided.
+     *
+     * @param sender  the message sender.
+     * @param message the new message to be added to the chat.
+     */
     public SendChatMessage(String sender, Message message) {
         super(Type.SEND_CHAT_MESSAGE, sender);
         this.message = message;
