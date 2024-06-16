89de2437be1e8f171f7b4f2a261f0334656e8cae
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:57:40 +0200

    feat(UpdateObjectiveCardMessage): add javadoc to UpdateObjectiveCardMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateObjectiveCardMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateObjectiveCardMessage.java
@@ -5,12 +5,22 @@ import it.polimi.ingsw.network.Type;
 import it.polimi.ingsw.network.client.model.card.ClientCard;
 import it.polimi.ingsw.network.client.model.card.ClientObjectiveCard;
 
+/**
+ * UpdateObjectiveCardMessage represents the message containing the chosen objective card with which to update.
+ */
 public class UpdateObjectiveCardMessage extends NetworkMessage {
 
     private final ClientObjectiveCard chosenObjective;
 
     private final String username;
 
+    /**
+     * Constructs an <code>UpdateObjectiveCardMessage</code> with the <code>chosenObjective</code> and
+     * <code>username</code> provided.
+     *
+     * @param chosenObjective the objective card chosen by the <code>username</code>.
+     * @param username        the username of the player who chose the <code>chosenObjective</code>.
+     */
     public UpdateObjectiveCardMessage(ClientObjectiveCard chosenObjective, String username) {
         super(Type.SHOW_UPDATE_OBJECTIVE_CARD, "server");
         this.chosenObjective = chosenObjective;
