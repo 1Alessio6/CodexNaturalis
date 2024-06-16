f9167670f63a96f7ee18955effb2402f0431dd3a
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:31:08 +0200

    feat(DrawMessage): add javadoc to DrawMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/DrawMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/DrawMessage.java
@@ -3,9 +3,18 @@ package it.polimi.ingsw.network.server.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * DrawMessage represents the message that allows a player to draw a card.
+ */
 public class DrawMessage extends NetworkMessage {
     private int idToDraw;
 
+    /**
+     * Constructs a <code>DrawMessage</code> with the <code>sender</code> and <code>idToDraw</code> provided.
+     *
+     * @param sender   the username of the player who performs the drawing.
+     * @param idToDraw the index of the card to be drawn.
+     */
     public DrawMessage(String sender, int idToDraw) {
         super(Type.DRAW, sender);
         this.idToDraw = idToDraw;
