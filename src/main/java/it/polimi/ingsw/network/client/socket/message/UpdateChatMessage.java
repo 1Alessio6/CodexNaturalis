2ab699781c09bdc9c04d8e9a75a9f98e2bcc0c7f
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:54:15 +0200

    feat(UpdateChatMessage): add javadoc to UpdateChatMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateChatMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateChatMessage.java
@@ -4,9 +4,19 @@ import it.polimi.ingsw.model.chat.message.Message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * UpdateChatMessage represents the message containing the message to <B>update</B> the chat.
+ */
 public class UpdateChatMessage extends NetworkMessage {
 
     private final Message message;
+
+    /**
+     * Constructs an <code>UpdateChatMessage</code> with the <code>message</code>.
+     * The sender is the server by default.
+     *
+     * @param message the new message to be displayed in chat.
+     */
     public UpdateChatMessage(Message message) {
         super(Type.SHOW_UPDATE_CHAT, "server");
         this.message = message;
