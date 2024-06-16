9718e5bcb4be1e7dbca86ba21c6d6e50a10aba08
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:30:08 +0200

    feat(DisconnectMessage): add javadoc to DisconnectMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/DisconnectMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/DisconnectMessage.java
@@ -3,7 +3,16 @@ package it.polimi.ingsw.network.server.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * DisconnectMessage represents the message that allows a player to disconnect from the game.
+ */
 public class DisconnectMessage extends NetworkMessage {
+
+    /**
+     * Constructs a <code>DisconnectMessage</code> with the <code>sender</code> provided.
+     *
+     * @param sender the username of the player to disconnect.
+     */
     public DisconnectMessage(String sender) {
         super(Type.DISCONNECT, sender);
     }
