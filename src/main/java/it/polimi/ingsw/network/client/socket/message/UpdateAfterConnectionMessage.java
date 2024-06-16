a55f76c31ad563c7e655284e6bc97a6c44e8d941
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:51:26 +0200

    feat(UpdateAfterConnectionMessage): add javadoc to UpdateAfterConnectionMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateAfterConnectionMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateAfterConnectionMessage.java
@@ -4,9 +4,19 @@ import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 import it.polimi.ingsw.network.client.model.ClientGame;
 
+/**
+ * UpdateAfterConnectionMessage represents the message containing the representation of the game after the connection
+ * of the number of players that was set at the beginning of the game.
+ */
 public class UpdateAfterConnectionMessage extends NetworkMessage {
 
     private final ClientGame game;
+
+    /**
+     * Constructs a <code>UpdateAfterConnectionMessage</code> with the <code>game</code> provided.
+     *
+     * @param game the representation of the game.
+     */
     public UpdateAfterConnectionMessage(ClientGame game) {
         super(Type.UPDATE_AFTER_CONNECTION, "server");
         this.game = game;
