f0f85fdd9daf37f3b304b9626f1e3dedc7e15b74
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:21:59 +0200

    feat(ChooseColorMessage): add javadoc to ChooseColorMessage class

--- src/main/java/it/polimi/ingsw/network/server/socket/message/ChooseColorMessage.java
+++ src/main/java/it/polimi/ingsw/network/server/socket/message/ChooseColorMessage.java
@@ -4,9 +4,18 @@ import it.polimi.ingsw.model.card.Color.PlayerColor;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * ChooseColorMessage represents the message containing the color chosen by the player.
+ */
 public class ChooseColorMessage extends NetworkMessage {
     private PlayerColor color;
 
+    /**
+     * Constructs a <code>ChooseColorMessage</code> with the <code>sender</code> and <code>color</code> provided.
+     *
+     * @param sender the username of the player who chose the color.
+     * @param color  the color chosen by the <code>sender</code>.
+     */
     public ChooseColorMessage(String sender, PlayerColor color) {
         super(Type.CHOOSE_COLOR, sender);
         this.color = color;
