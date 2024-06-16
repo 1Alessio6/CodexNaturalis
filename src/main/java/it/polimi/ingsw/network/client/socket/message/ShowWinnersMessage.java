a5bd70eb147629dd25bb0c2e9b5cb695568cd168
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:46:47 +0200

    feat(ShowWinnersMessage): add javadoc to ShowWinnersMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/ShowWinnersMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/ShowWinnersMessage.java
@@ -5,9 +5,18 @@ import it.polimi.ingsw.network.Type;
 
 import java.util.List;
 
+/**
+ * ShowWinnersMessage represents the message containing the winners of the game.
+ */
 public class ShowWinnersMessage extends NetworkMessage {
 
     private final List<String> winners;
+
+    /**
+     * Constructs <code>ShowWinnerMessage</code> with the <code>winners</code> provided.
+     *
+     * @param winners the winners of the game.
+     */
     public ShowWinnersMessage(List<String> winners) {
         super(Type.SHOW_WINNERS, "server");
         this.winners = winners;
