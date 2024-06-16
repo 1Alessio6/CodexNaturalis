767f85363c854864c62d84a9e14eee1b7354519b
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:59:21 +0200

    feat(UpdatePlayerStatusMessage): add javadoc to UpdatePlayerStatusMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdatePlayerStatusMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdatePlayerStatusMessage.java
@@ -3,11 +3,20 @@ package it.polimi.ingsw.network.client.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * UpdatePlayerStatusMessage represents the message containing the status of the player.
+ */
 public class UpdatePlayerStatusMessage extends NetworkMessage {
 
     private final boolean isConnected;
     private final String username;
 
+    /**
+     * Constructs an <code>UpdatePlayerStatusMessage</code> with the status and <code>username</code> provided.
+     *
+     * @param isConnected the player status.
+     * @param username    the username of the player.
+     */
     public UpdatePlayerStatusMessage(boolean isConnected, String username) {
         super(Type.SHOW_UPDATE_PLAYER_STATUS, "server");
         this.isConnected = isConnected;
