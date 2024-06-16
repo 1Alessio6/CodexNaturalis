cb3dd25a49fa9e03c48cd9f66187f78880a77e4b
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-16 21:55:52 +0200

    feat(UpdateCreatorMessage): add javadoc to UpdateCreatorMessage class

--- src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateCreatorMessage.java
+++ src/main/java/it/polimi/ingsw/network/client/socket/message/UpdateCreatorMessage.java
@@ -3,7 +3,13 @@ package it.polimi.ingsw.network.client.socket.message;
 import it.polimi.ingsw.network.NetworkMessage;
 import it.polimi.ingsw.network.Type;
 
+/**
+ * UpdateCreatorMessage represents the message that executes the lobby after the arrival of the first player.
+ */
 public class UpdateCreatorMessage extends NetworkMessage {
+    /**
+     * Constructs an <code>UpdateCreatorMessage</code> message.
+     */
     public UpdateCreatorMessage() {
         super(Type.UPDATE_CREATOR, "server");
     }
