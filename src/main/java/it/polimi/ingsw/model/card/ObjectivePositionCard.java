bd1a7128302ef0159ab531e97f2f5f479295ebce
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 17:00:02 +0200

    feat(ObjectivePositionCard): complete ObjectivePositionCard javadoc

--- src/main/java/it/polimi/ingsw/model/card/ObjectivePositionCard.java
+++ src/main/java/it/polimi/ingsw/model/card/ObjectivePositionCard.java
@@ -14,6 +14,9 @@ import java.util.List;
 public class ObjectivePositionCard extends ObjectiveCard {
     private final Map<Position, CardColor> condition;
 
+    /**
+     * Constructs an objective position card with no parameter provided
+     */
     public ObjectivePositionCard() {
         super();
         condition = new HashMap<>();
@@ -106,6 +109,12 @@ public class ObjectivePositionCard extends ObjectiveCard {
         return count;
     }
 
+    /**
+     * Checks if two objects are equal, two objective position cards in particular
+     *
+     * @param o the object to be compared
+     * @return true if this object is equal to <code>o</code>, otherwise false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -115,6 +124,9 @@ public class ObjectivePositionCard extends ObjectiveCard {
         return Objects.equals(condition, that.condition);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), condition);
