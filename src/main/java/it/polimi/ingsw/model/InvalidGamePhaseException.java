d2a926c693b23476120a4f36b0ecb294f3c27f49
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:50:59 +0200

    feat(InvalidGamePhaseException): add InvalidGamePhaseException constructors javadoc

--- src/main/java/it/polimi/ingsw/model/InvalidGamePhaseException.java
+++ src/main/java/it/polimi/ingsw/model/InvalidGamePhaseException.java
@@ -4,10 +4,18 @@ package it.polimi.ingsw.model;
  * Invalid Game Phase Exception is thrown when an attempt is made to perform an action that is not allowed at a given phase of the game
  */
 public class InvalidGamePhaseException extends Exception {
+    /**
+     * Constructs an <code>InvalidGamePhaseException</code> with no detail message
+     */
     public InvalidGamePhaseException() {
         super("Invalid game phase");
     }
 
+    /**
+     * Constructs an <code>InvalidGamePhaseException</code> with the <code>message</code> provided
+     *
+     * @param message the detail message
+     */
     public InvalidGamePhaseException(String message) {
         super(message);
     }
