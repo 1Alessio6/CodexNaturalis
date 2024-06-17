4a97fd8502080f8c0fc8c262b5abf924b7bb0525
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 17:02:25 +0200

    feat(PhaseHandlerTest): add PhaseHandlerTest javadoc

--- src/test/java/it/polimi/ingsw/model/gamePhase/PhaseHandlerTest.java
+++ src/test/java/it/polimi/ingsw/model/gamePhase/PhaseHandlerTest.java
@@ -13,10 +13,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Test to check the correct game phase transition
+ */
 public class PhaseHandlerTest {
     private List<Player> genericPlayers;
     private PhaseHandler phaseHandler;
 
+    /**
+     * Creates new base and objective cards and instances of the <code>genericPlayers</code> and the
+     * <code>phaseHandler</code> before each test
+     */
     @BeforeEach
     void setUp() {
         List<Card> cards = new ArrayList<>(Arrays.asList(
@@ -44,11 +51,17 @@ public class PhaseHandlerTest {
         phaseHandler = new PhaseHandler(genericPlayers.size());
     }
 
+    /**
+     * Tests the transition from beginning to set up phase
+     */
     @Test
     void transitionFromSetup_nextPhaseSetup() {
         Assertions.assertEquals(GamePhase.Setup, phaseHandler.getNextPhase(GamePhase.Setup, 0));
     }
 
+    /**
+     * Tests the transition from set up to place normal phase
+     */
     @Test
     void transitionFromSetup_nextPhasePlaceNormal() {
         var ref = new Object() {
@@ -69,16 +82,28 @@ public class PhaseHandlerTest {
         Assertions.assertEquals(GamePhase.PlaceNormal, ref.phase);
     }
 
+    /**
+     * Tests the transition from place normal to draw normal phase
+     */
     @Test
     void transitionFromPlaceNormal_nextPhaseDrawNormal() {
         Assertions.assertEquals(GamePhase.DrawNormal, phaseHandler.getNextPhase(GamePhase.PlaceNormal, 0));
     }
 
+    /**
+     * Tests the transition from draw normal to place normal phase
+     */
     @Test
     void transitionFromDrawNormal_nextPhasePlaceNormal() {
         Assertions.assertEquals(GamePhase.PlaceNormal, phaseHandler.getNextPhase(GamePhase.DrawNormal, 0));
     }
 
+    /**
+     * Tests the correct phase transition in last normal turn case
+     *
+     * @param playerCausingLastNormalTurn the player who causes the last normal turn
+     * @param phase                       the game phase
+     */
     private void testLastTurn(int playerCausingLastNormalTurn, GamePhase phase) {
         for (int playerNormalTurn = playerCausingLastNormalTurn + 1; playerNormalTurn < genericPlayers.size(); ++playerNormalTurn) {
             Assertions.assertEquals(GamePhase.PlaceNormal, phase);
@@ -90,6 +115,10 @@ public class PhaseHandlerTest {
         Assertions.assertEquals(phase, GamePhase.PlaceAdditional);
     }
 
+    /**
+     * Tests the transition from draw normal to place additional phase in case a player reached 20 points or the decks
+     * become empty
+     */
     @Test
     void transitionFromDrawNormal_nextPhasePlaceAdditional() {
         for (int playerCausingLastNormalTurn = 0; playerCausingLastNormalTurn < genericPlayers.size(); ++playerCausingLastNormalTurn) {
@@ -111,6 +140,9 @@ public class PhaseHandlerTest {
         }
     }
 
+    /**
+     * Tests the transition from additional turn to the end phase
+     */
     @Test
     void testTransitionsFromAdditionalTurn() {
         GamePhase phase = GamePhase.PlaceAdditional;
