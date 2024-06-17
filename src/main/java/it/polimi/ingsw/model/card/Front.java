a23011bb19f8e65aa0900f2b12183e72097df70e
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:43:51 +0200

    feat(Front): complete Front javadoc

--- src/main/java/it/polimi/ingsw/model/card/Front.java
+++ src/main/java/it/polimi/ingsw/model/card/Front.java
@@ -8,9 +8,15 @@ import it.polimi.ingsw.model.card.strategies.CalculatePoints;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Front is a class that extends Face and represents the front of the face
+ */
 public class Front extends Face {
     private final int score;
 
+    /**
+     * Constructs a front card with no parameters provided.
+     */
     public Front() {
         super();
         score = 0;
@@ -34,6 +40,15 @@ public class Front extends Face {
         this.score = score;
     }
 
+    /**
+     * Constructs a front card with the color, corners, score and calculator provided.
+     *
+     * @param color      the color of the card
+     * @param corners    the corners of the card
+     * @param score      the score obtained after positioning the card
+     * @param calculator the calculator of the card
+     * @throws IllegalArgumentException if any argument is null, there aren't 4 corners or the score is negative
+     */
     public Front(CardColor color, Map<CornerPosition, Corner> corners, int score, CalculatePoints calculator) throws IllegalArgumentException {
         super(color, corners, calculator);
 
@@ -70,6 +85,11 @@ public class Front extends Face {
         return score;
     }
 
+    /**
+     * Facilitates the deserialization of the different cards present in the json.
+     *
+     * @return modified string
+     */
     public String toString() {
 
         StringBuilder cornerString = new StringBuilder();
@@ -84,6 +104,12 @@ public class Front extends Face {
         return "{ Color: " + this.getColor() + "| Points" + this.score + " ) " + "| Corners: [ " + cornerString + " ] ";
     }
 
+    /**
+     * Checks if two objects are equal, two fronts in particular.
+     *
+     * @param o the object to be compared
+     * @return true if this object is equal to <code>o</code>, otherwise false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -93,6 +119,13 @@ public class Front extends Face {
         return score == front.score;
     }
 
+    /**
+     * Returns a hashcode depending on the attributes.
+     * Given two instance with the same attributes this method returns the same hashcode.
+     * Overrides the hashCode() method in the Object class.
+     *
+     * @return an int value representing the hashcode of the object.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), score);
