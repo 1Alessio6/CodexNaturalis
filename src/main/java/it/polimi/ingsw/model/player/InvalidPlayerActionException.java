194113b7ba0630640a8fc8df2f9b41847f08fedf
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:53:04 +0200

    feat(InvalidPlayerActionException): add InvalidPlayerActionException constructors javadoc

--- src/main/java/it/polimi/ingsw/model/player/InvalidPlayerActionException.java
+++ src/main/java/it/polimi/ingsw/model/player/InvalidPlayerActionException.java
@@ -4,10 +4,18 @@ package it.polimi.ingsw.model.player;
  * Invalid Player Action Exception  is thrown when the player tries to perform an action that is not allowed
  */
 public class InvalidPlayerActionException extends Exception {
+    /**
+     * Constructs an <code>InvalidPlayerActionException</code> with no detail message
+     */
     public InvalidPlayerActionException()  {
         super("Invalid player action");
     }
 
+    /**
+     * Constructs an <code>InvalidPlayerActionException</code> with the <code>message</code> provided
+     *
+     * @param message the detail message
+     */
     public InvalidPlayerActionException(String message) {
         super(message);
     }
