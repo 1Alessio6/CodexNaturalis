c156ae152e4fd9f465ff0ff52673a1b59dac5f37
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:49:36 +0200

    feat(InvalidColorException): add InvalidColorException constructors javadoc

--- src/main/java/it/polimi/ingsw/model/card/Color/InvalidColorException.java
+++ src/main/java/it/polimi/ingsw/model/card/Color/InvalidColorException.java
@@ -4,10 +4,18 @@ package it.polimi.ingsw.model.card.Color;
  * Invalid Color Exception is thrown when the player attempts to select a color that has already been selected
  */
 public class InvalidColorException extends Exception {
+    /**
+     * Constructs an <code>InvalidColorException</code> with no detail message.
+     */
     public InvalidColorException() {
         super("Invalid color");
     }
 
+    /**
+     * Constructs an <code>InvalidColorException</code> with the <code>message</code> provided
+     *
+     * @param message the detail message
+     */
     public InvalidColorException(String message) {
         super(message);
     }
