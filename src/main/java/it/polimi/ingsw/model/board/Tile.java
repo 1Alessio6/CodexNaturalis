ec4c051dd886cbdeb50b791dc0cf09f825ca69a3
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 17:11:51 +0200

    feat(Tile): complete Tile javadoc

--- src/main/java/it/polimi/ingsw/model/board/Tile.java
+++ src/main/java/it/polimi/ingsw/model/board/Tile.java
@@ -8,7 +8,7 @@ import it.polimi.ingsw.model.card.Face;
 
 public class Tile {
     private Face face;
-    //private boolean isUsed; a variable used to check if the card has been already used for redeeming the points of an objective card
+
     private Availability availability;
 
     public Availability getAvailability() {
@@ -17,17 +17,18 @@ public class Tile {
 
     /**
      * Constructs a tile which doesn't contain a face with the parameter's availability.
+     *
      * @param a the tile's availability.
      */
-    public Tile(Availability a){ //this constructor method it's used when we create an empty Tile that could be available or not available
+    public Tile(Availability a) { //this constructor method it's used when we create an empty Tile that could be available or not available
         face = null;
         availability = a;
     }
 
 
-
     /**
      * Returns the face contained in the tile.
+     *
      * @return a face representing the card's face placed in the tile.
      */
     public Face getFace() {
@@ -35,9 +36,9 @@ public class Tile {
     }
 
 
-
     /**
      * Checks if the availability of the tile it's equal to parameter's availability.
+     *
      * @param availability the availability compared.
      */
     public boolean sameAvailability(Availability availability) {
@@ -45,9 +46,9 @@ public class Tile {
     }
 
 
-
     /**
      * Sets the availability.
+     *
      * @param availability the new availability of the tile.
      */
     public void setAvailability(Availability availability) {
@@ -55,18 +56,23 @@ public class Tile {
     }
 
 
-
     /**
      * Sets the availability.
+     *
      * @param face the new face contained in the tile.
      */
     public void setFace(Face face) {
         this.face = face;
     }
 
-    public String toString(){
+    /**
+     * Returns a string representation of the tile, including its availability and the eventual face placed in it.
+     *
+     * @return a string representing the tile
+     */
+    public String toString() {
         String face = "EmptyTile";
-        if(this.getFace() != null){
+        if (this.getFace() != null) {
             face = String.valueOf(this.getFace());
         }
         return availability + "  " + face;
