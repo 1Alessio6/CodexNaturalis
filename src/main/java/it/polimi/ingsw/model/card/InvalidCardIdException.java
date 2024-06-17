e7748722d79900f70adc05c78811377a097c4733
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:48:49 +0200

    feat(InvalidCardIdException): add InvalidCardIdException constructor javadoc

--- src/main/java/it/polimi/ingsw/model/card/InvalidCardIdException.java
+++ src/main/java/it/polimi/ingsw/model/card/InvalidCardIdException.java
@@ -4,6 +4,9 @@ package it.polimi.ingsw.model.card;
  * Invalid Card ID Exception is thrown when the provided id doesn't match with any existing card.
  */
 public class InvalidCardIdException extends Exception {
+    /**
+     * Constructs an <code>InvalidCardIdException</code> with no detail message.
+     */
     public InvalidCardIdException() {
         super("Illegal card id");
     }
