4407891e5388cadb1047cc6a63e5188214ff596c
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:50:19 +0200

    feat(InvalidFaceUpCardException): add InvalidFaceUpCardException constructor javadoc

--- src/main/java/it/polimi/ingsw/model/card/InvalidFaceUpCardException.java
+++ src/main/java/it/polimi/ingsw/model/card/InvalidFaceUpCardException.java
@@ -4,6 +4,11 @@ package it.polimi.ingsw.model.card;
  * Invalid FaceUp Card Exception is thrown when the provided id doesn't match with any face up card.
  */
 public class InvalidFaceUpCardException extends Exception {
+    /**
+     * Constructs an <code>InvalidFaceUpCardException</code> with the <code>message</code> provided.
+     *
+     * @param message the detail message
+     */
     public InvalidFaceUpCardException(String message)  {
         super(message);
     }
