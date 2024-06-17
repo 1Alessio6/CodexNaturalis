aa82dde3a4d46d5f4fa2493e6b03a7f288582d37
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:26:33 +0200

    feat(BackTest): add BackTest javadoc

--- src/test/java/it/polimi/ingsw/model/card/BackTest.java
+++ src/test/java/it/polimi/ingsw/model/card/BackTest.java
@@ -6,7 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
+/**
+ * Test to check whether two back cards match or not, depending on their attributes.
+ */
 public class BackTest {
+    /**
+     * Test to check if two back cards are the same depending on their attributes.
+     */
     @Test
     public void sameAttributes_sameBack() {
         Back b1 = new Back(CardColor.BLUE, new HashMap<>(), new HashMap<>());
@@ -15,6 +21,9 @@ public class BackTest {
         Assertions.assertEquals(b1, b2);
     }
 
+    /**
+     * Test to check if two back cards are the different depending on their attributes.
+     */
     @Test
     public void diffAttributes_differentBack() {
         Back b1 = new Back(CardColor.BLUE, new HashMap<>(), new HashMap<>());
