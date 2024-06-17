103a18c9007bdd86878a91173f2a0432d5250710
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:51:48 +0200

    feat(InvalidIdForDrawingException): add InvalidIdForDrawingException constructor javadoc

--- src/main/java/it/polimi/ingsw/controller/InvalidIdForDrawingException.java
+++ src/main/java/it/polimi/ingsw/controller/InvalidIdForDrawingException.java
@@ -4,6 +4,9 @@ package it.polimi.ingsw.controller;
  * Invalid ID For Drawing Exception is thrown when the card id isn't valid for drawing
  */
 public class InvalidIdForDrawingException extends Exception {
+    /**
+     * Constructs an <code>InvalidIdForDrawingException</code> with no detail message.
+     */
     public InvalidIdForDrawingException() {
         super("Invalid id for drawing");
     }
