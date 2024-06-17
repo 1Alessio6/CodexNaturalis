6421251fbec7a735d94c9447143ece6ad952c8ca
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-17 16:29:19 +0200

    feat(CalculatePoints: complete CalculatePoints javadoc

--- src/main/java/it/polimi/ingsw/model/card/strategies/CalculatePoints.java
+++ src/main/java/it/polimi/ingsw/model/card/strategies/CalculatePoints.java
@@ -7,5 +7,12 @@ import it.polimi.ingsw.model.board.*;
  * depending on the particular encountered.
  */
 public interface CalculatePoints {
+    /**
+     * Calculates the points earned after the placement of a card.
+     *
+     * @param pos        the position in the playground.
+     * @param playground the playground on which the calculation is to be carried.
+     * @return the calculated points.
+     */
     public int calculatePoints (Position pos, Playground playground);
 }
