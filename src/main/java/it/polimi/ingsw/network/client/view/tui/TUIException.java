7ad921614de24f5bc5669fc6f7764b730b3655f7
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-18 15:35:16 +0200

    feat(TUIActions): add javadoc to TUIException constructor

--- src/main/java/it/polimi/ingsw/network/client/view/tui/TUIException.java
+++ src/main/java/it/polimi/ingsw/network/client/view/tui/TUIException.java
@@ -4,6 +4,11 @@ package it.polimi.ingsw.network.client.view.tui;
  * TUI Exception is thrown when an error occurs during the TUI running
  */
 public class TUIException extends Exception {
+    /**
+     * Constructs a <code>TUIException</code> with the <code>exception</code> provided
+     *
+     * @param exception the detail exception
+     */
     public TUIException(ExceptionsTUI exception) {
         super(exception.getMessage());
     }
