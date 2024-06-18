4b75ef0133e23f930f47c5527af75de42d17bc78
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-18 15:35:58 +0200

    feat(UnReachableServerException): add javadoc to UnReachableServerException

--- src/main/java/it/polimi/ingsw/network/client/UnReachableServerException.java
+++ src/main/java/it/polimi/ingsw/network/client/UnReachableServerException.java
@@ -4,6 +4,9 @@ package it.polimi.ingsw.network.client;
  * Unreachable Server Exception is thrown when the server isn't accessible.
  */
 public class UnReachableServerException extends Exception {
+    /**
+     * Constructs an <code>UnReachableServerException</code> with no detail message.
+     */
     public UnReachableServerException() {
         super("Unreachable server");
     }
