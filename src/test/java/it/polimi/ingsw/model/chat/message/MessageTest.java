b3357eb15f30d001d53f65d3334f18d2be5187d0
Natalia <112645876+audlo@users.noreply.github.com>
2024-06-18 03:45:13 +0200

    feat(MessageTest): add MessageTest javadoc

--- src/test/java/it/polimi/ingsw/model/chat/message/MessageTest.java
+++ src/test/java/it/polimi/ingsw/model/chat/message/MessageTest.java
@@ -3,7 +3,14 @@ package it.polimi.ingsw.model.chat.message;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+/**
+ * Test to check the correct passage of the parameters in order to instance a <code>Message</code> and the correct
+ * creation of a broadcast and not broadcast message
+ */
 class MessageTest {
+    /**
+     * Test to check that passing a valid reference does not throw any kind of exception
+     */
     @Test
     public void passValidReference_doesNotThrow() {
         Assertions.assertDoesNotThrow(() -> {
@@ -15,16 +22,25 @@ class MessageTest {
         });
     }
 
+    /**
+     * Test to check that a message created for a specific username isn't a message in broadcast
+     */
     @Test
     public void createMessageForSpecificUsername_notBroadcast() {
         Assertions.assertFalse(new Message("a", "b", "c").isBroadcast());
     }
 
+    /**
+     * Test to check that a message created for all users is a message in broadcast
+     */
     @Test
     public void createMessageForAllUsers_isBroadcast() {
         Assertions.assertTrue(new Message("a", "c").isBroadcast());
     }
 
+    /**
+     * Test to check that passing a null sender throws an <code>IllegalArgumentException</code>
+     */
     @Test
     public void passNullSender_throwsException() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
@@ -32,6 +48,9 @@ class MessageTest {
         });
     }
 
+    /**
+     * Test to check that passing a null receiver throws an <code>IllegalArgumentException</code>
+     */
     @Test
     public void passNullReceiver_throwsException() {
         Assertions.assertThrows(
@@ -41,6 +60,9 @@ class MessageTest {
                 });
     }
 
+    /**
+     * Test to check that passing a null content throws an <code>IllegalArgumentException</code>
+     */
     @Test
     public void passNullContent_throwsException() {
         Assertions.assertThrows(
