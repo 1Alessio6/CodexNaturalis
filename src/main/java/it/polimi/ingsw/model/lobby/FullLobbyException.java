264f65b29ea70879c9c0051af1122e96e5758624
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-06-26 17:23:02 +0200

    refactor: change message in FullLobbyException

--- src/main/java/it/polimi/ingsw/model/lobby/FullLobbyException.java
+++ src/main/java/it/polimi/ingsw/model/lobby/FullLobbyException.java
@@ -8,6 +8,6 @@ public class FullLobbyException extends Exception {
      * Constructs a <code>FullLobbyException</code> with no detail message.
      */
     public FullLobbyException() {
-        super("Game hasn't started yet, but lobby is already full");
+        super("Lobby is already full");
     }
 }
