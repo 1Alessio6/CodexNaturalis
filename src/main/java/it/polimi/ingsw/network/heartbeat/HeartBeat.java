8bd11aebe4dab91a052ef3d6c7b271ae09008794
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-06-26 17:52:23 +0200

    refactor(HeartBeat): make the Heartbeat a thread and add a timer to handle unresponsiveness
    Make the heartbeat a thread since it's equivalent to a synchronized function that blocks all other threads spawned by the scheduler from executing.
    Add a timer to not being stuck on a remote call which may last very long when the connection is turned off.

--- src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeat.java
+++ src/main/java/it/polimi/ingsw/network/heartbeat/HeartBeat.java
@@ -9,16 +9,15 @@ import java.util.concurrent.atomic.AtomicInteger;
  * The HeartBeat class keeps track of the existence of the remote endpoint which can be the server for the client or the client for the server.
  * The HeartBeat must always reflect the state of the connection, that is, it has to be terminated {@link #terminate()} whenever a connection ends.
  */
-public class HeartBeat extends TimerTask {
+public class HeartBeat extends Thread {
     private String handlerName;
+    private final Object handlerNameLock;
     private String listenerName;
     private HeartBeatHandler heartBeatHandler;
     private HeartBeatListener heartBeatListener;
-    private static final int MAX_DELTA = 3;
-    private static final int DEF_HEART_BEAT_PERIOD = 1000; // ms
-    private int heartBeatPeriod;
-    private int delay;
-    private final Timer timer;
+    private static final int MAX_DELTA = 3; // 3
+    private static final int HEART_BEAT_PERIOD = 1000;
+    private static final int MAX_DELAY = 5000; // 5000
     private AtomicInteger mostRecentReceivedId;
     private Integer lastSentId;
     private AtomicBoolean isActive;
@@ -26,86 +25,89 @@ public class HeartBeat extends TimerTask {
 
     /**
      * Constructs the heart beat.
-     * @param heartBeatHandler the local end point of the connection.
-     * @param handlerName the identifier for the handler.
+     *
+     * @param heartBeatHandler  the local end point of the connection.
+     * @param handlerName       the identifier for the handler.
      * @param heartBeatListener the remote end point of the connection.
-     * @param listenerName the identifier for the listener.
+     * @param listenerName      the identifier for the listener.
      */
     public HeartBeat(HeartBeatHandler heartBeatHandler, String handlerName, HeartBeatListener heartBeatListener, String listenerName) {
         this.heartBeatHandler = heartBeatHandler;
         this.handlerName = handlerName;
         this.heartBeatListener = heartBeatListener;
+        this.handlerNameLock = new Object();
         this.listenerName = listenerName;
         this.mostRecentReceivedId = new AtomicInteger();
-        this.heartBeatPeriod = DEF_HEART_BEAT_PERIOD;
-        this.delay = 0;
         this.lastSentId = 0;
-        timer = new Timer();
         isActive = new AtomicBoolean(true);
-    }
-
-    public synchronized void setHeartBeatPeriod(int heartBeatPeriod) {
-        if (heartBeatPeriod < 0) {
-            throw new IllegalArgumentException("Invalid heartbeat period: negative value not allowed");
-        }
-        this.heartBeatPeriod = heartBeatPeriod;
-    }
-
-    public synchronized void setDelay(int delay) {
-        this.delay = delay;
     }
 
     /**
      * Sets the name of the handler.
+     *
      * @param name of the handler.
      */
-    public synchronized void setHandlerName(String name) {
-        this.handlerName = name;
+    public void setHandlerName(String name) {
+        synchronized (handlerNameLock) {
+            this.handlerName = name;
+        }
     }
 
     /**
      * Starts sending the heart beat to the remote end point.
      */
-    public synchronized void startHeartBeat() {
-      //  isActive = true;
-        timer.scheduleAtFixedRate(this, delay, heartBeatPeriod);
+    public void startHeartBeat() {
+        this.start();
     }
-
- //   public synchronized void setListener(HeartBeatListener heartBeatListener, String listenerName) {
- //       this.heartBeatListener = heartBeatListener;
- //       this.listenerName = listenerName;
- //   }
 
     /**
      * Registers the arrival of a message from the remote end point.
-     * @param pong the message notify the existence of the remote endpoint.
+     *
+     * @param ping the message notify the existence of the remote endpoint.
      */
-    public void registerMessage(HeartBeatMessage pong) {
-        mostRecentReceivedId.set(pong.getId());
+    public void registerMessage(HeartBeatMessage ping) {
+        mostRecentReceivedId.set(ping.getId());
     }
 
     /**
      * Sends the heart beat to the remote reference notifying about the existence of the local end point.
      */
     @Override
-    public synchronized void run() {
-        lastSentId += 1;
-        HeartBeatMessage ping = new HeartBeatMessage(handlerName, lastSentId);
-        int delta = ping.getId() - mostRecentReceivedId.get();
+    public void run() {
+        while (!isInterrupted()) {
+            lastSentId += 1;
+            HeartBeatMessage ping;
+            synchronized (handlerNameLock) {
+                ping = new HeartBeatMessage(handlerName, lastSentId);
+            }
+            int delta = ping.getId() - mostRecentReceivedId.get();
+            System.err.println("Current delta: " + delta + " for " + handlerName + " to " + listenerName);
 
-        if (!isActive.get()) {
-            return;
-        }
-
-        if (delta <= MAX_DELTA) {
-            try {
-                heartBeatListener.receivePing(ping);
-            } catch (RemoteException e) {
-                System.err.println("Remote exception in Heartbeat: the listener has disconnected for this reason: " + e.getMessage());
+            if (delta <= MAX_DELTA) {
+                try {
+                    Timer timerDelayForSendingPing = new Timer();
+                    timerDelayForSendingPing.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            System.out.println("Timer for sending ping of " + listenerName + " has expired");
+                            heartBeatHandler.handleUnresponsiveness(listenerName);
+                        }
+                    }, MAX_DELAY);
+                    heartBeatListener.receivePing(ping);
+                    timerDelayForSendingPing.cancel();
+                } catch (RemoteException e) {
+                    System.err.println("REMOTE EXCEPTION in Heartbeat: the listener has disconnected for this reason: " + e.getMessage());
+                    heartBeatHandler.handleUnresponsiveness(listenerName);
+                }
+            } else {
+                System.err.println("Delta for " + listenerName + " is too high: last_delta=" + delta);
                 heartBeatHandler.handleUnresponsiveness(listenerName);
             }
-        } else {
-            heartBeatHandler.handleUnresponsiveness(listenerName);
+            try {
+                Thread.sleep(HEART_BEAT_PERIOD);
+            } catch (InterruptedException e) {
+                break;
+            }
         }
     }
 
@@ -115,14 +117,14 @@ public class HeartBeat extends TimerTask {
      * All references to external objects become invalid to avoid running task to interfere with the state of such objects.
      */
     public void terminate() {
+        System.err.println("Terminate the heartbeat");
+        this.interrupt();
         isActive.set(false);
-        synchronized (this) {
-            timer.cancel();
-        }
     }
 
     /**
      * Returns the state of the heartbeat.
+     *
      * @return true if the heart beat is valid, false otherwise.
      * The heart beat is valid if it's never terminated.
      */
