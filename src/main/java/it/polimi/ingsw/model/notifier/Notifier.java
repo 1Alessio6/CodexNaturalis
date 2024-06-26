62fef747fff24abbb4f6d165f5ce540ddd6ebb44
Riccardo Cerberi <riccardo.cerberi@mail.polimi.it>
2024-06-26 18:15:00 +0200

    refactor(Notifier): remove the RemoteException
    Remove the RemoteException because now the handler takes care of network problems.

--- src/main/java/it/polimi/ingsw/model/notifier/Notifier.java
+++ src/main/java/it/polimi/ingsw/model/notifier/Notifier.java
@@ -1,9 +1,5 @@
 package it.polimi.ingsw.model.notifier;
 
-import it.polimi.ingsw.network.VirtualView;
-
-import java.rmi.RemoteException;
-
 public interface Notifier<ReceiverType> {
-    public void sendUpdate(ReceiverType receiver) throws RemoteException;
+    void sendUpdate(ReceiverType receiver);
 }
