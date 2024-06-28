d464b32136a77451b9f91db2d1d219b5139e9bf4
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2024-06-28 03:27:25 +0200

    fix(ClientUtil): reduce scoreboard size, to make playground not overlap to chat

--- src/main/java/it/polimi/ingsw/network/client/view/tui/ClientUtil.java
+++ src/main/java/it/polimi/ingsw/network/client/view/tui/ClientUtil.java
@@ -28,14 +28,14 @@ import static it.polimi.ingsw.network.client.view.tui.ClientUtil.cardWidth;
  */
 enum GameScreenArea {
     // Matrix (12-1) * (20-1) cards (considering last not overlapping)
-    PLAYGROUND(97, 41, new Position(28, 2)),
+    PLAYGROUND(97, 41, new Position(27, 2)),
     FACE_UP_CARDS(24, 14, new Position(146, 2)),
     HAND_CARDS(2* ClientUtil.areaPadding + 3* cardWidth, cardHeight, new Position(62 ,44)),
     DECKS(24, 5, new Position(18, 149)),
     CHAT(62, 11, new Position(23, 126)),
     INPUT_AREA(62,12,new Position(35,126)),
     TITLE(80,5,new Position(2,55)),
-    SCOREBOARD(10, 26, new Position(2, 2)),
+    SCOREBOARD(10, 24, new Position(2, 2)),
     PRIVATE_OBJECTIVE(ClientUtil.objectiveCardWidth, ClientUtil.objectiveCardHeight, new Position(35, 2)),
     COMMON_OBJECTIVE(2 + 2 * ClientUtil.objectiveCardWidth, ClientUtil.objectiveCardHeight, new Position(42, 2)),
     RESOURCES(26, 15, new Position(14, 2));
@@ -72,7 +72,7 @@ public class ClientUtil {
     public final static int objectiveCardWidth = 11;
     public final static int objectiveCardHeight = 5;
     final static int areaPadding = 2;
-    final static int maxUsernameLength=15;
+    final static int maxUsernameLength=12;
     final static int maxPointsLength=8;
     final static int resourceBoardColumnSpace = 5;
     final static int requirementsHeight = 2;
